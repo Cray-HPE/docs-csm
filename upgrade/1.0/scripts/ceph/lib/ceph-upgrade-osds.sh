9795abba435493113ab1e095c7aec527e85b406f
Harold Longley <htg@cray.com>
2021-05-18 08:04:03 -0500

    CASMINST-2272 with minor update from Craig for ceph-upgrade-osds.sh

--- upgrade/1.0/scripts/ceph/lib/ceph-upgrade-osds.sh
+++ upgrade/1.0/scripts/ceph/lib/ceph-upgrade-osds.sh
@@ -9,6 +9,8 @@ for host in $(ceph node ls| jq -r '.osd|keys[]')
   for osd in $(ceph node ls| jq --arg host_key $host -r '.osd[$host_key]|values|tostring|ltrimstr("[")|rtrimstr("]")'| sed "s/,/ /g")
    do
     ssh $host "cephadm --image $registry/ceph/ceph:v15.2.8 adopt --style legacy --name osd.$osd" --skip-pull
+    ceph mgr fail $(ceph mgr dump | jq -r .active_name)
+    sleep 20
    done
  done
 }
