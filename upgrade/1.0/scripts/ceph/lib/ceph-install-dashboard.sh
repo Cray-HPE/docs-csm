38a000c51c43152d44256a0d98ad73b555dba95b
Craig DeLatte <cdelatte@cray.com>
2021-06-22 18:58:55 -0400

    CASMINST-2491 remove clear text passwd
    This script is still a work in progress that got carried over from
    some early work.  This will at least remove the passwd from the docs

--- upgrade/1.0/scripts/ceph/lib/ceph-install-dashboard.sh
+++ upgrade/1.0/scripts/ceph/lib/ceph-install-dashboard.sh
@@ -15,22 +15,28 @@ function install_dashboard () {
     then
         ceph config set mgr/dashboard/server_port 8443
     fi
-    echo "Creating cray_cephadm dashboard user"
-    ceph dashboard ac-user-create cray_cephadm initial0 administrator
-    echo "Setting up dashboard access to radosgw"
-    radosgw-admin user create --uid=cray_cephadm --display-name=cray_cephadm --system
-    access_key=$(radosgw-admin user info --uid cray_cephadm|jq '.keys[0].access_key')
-    secret_key=$(radosgw-admin user info --uid cray_cephadm|jq '.keys[0].secret_key')
-    ceph dashboard set-rgw-api-access-key -i "$access_key"
-    ceph dashboard set-rgw-api-secret-key -i "$secret_key"
-    # Leaving a place where we can set the rgw-vip address
-    # ceph dashboard set-rgw-api-host <host>
-    ceph dashboard set-rgw-api-port 8080
-    # Putting option incase we need to enable/disable https
-    # ceph dashboard set-rgw-api-scheme <scheme>  # http or https
-    # Need to investigate the below
-    # ceph dashboard set-rgw-api-admin-resource <admin_resource>
-    echo "Disable ssl_verify until we are on signed certs"
-    ceph dashboard set-rgw-api-ssl-verify False
-    # Add checks for verifying the dashboard is up and functional
+    read -s -p "Enter a password for the initial deployment" passwd
+    read  -s -p "Confirm passwd" passwd2
+    if [[ $passwd == $passwd2 ]] then
+      echo "Creating cray_cephadm dashboard user"
+      ceph dashboard ac-user-create cray_cephadm $passwd administrator
+      echo "Setting up dashboard access to radosgw"
+      radosgw-admin user create --uid=cray_cephadm --display-name=cray_cephadm --system
+      access_key=$(radosgw-admin user info --uid cray_cephadm|jq '.keys[0].access_key')
+      secret_key=$(radosgw-admin user info --uid cray_cephadm|jq '.keys[0].secret_key')
+      ceph dashboard set-rgw-api-access-key -i "$access_key"
+      ceph dashboard set-rgw-api-secret-key -i "$secret_key"
+      # Leaving a place where we can set the rgw-vip address
+      # ceph dashboard set-rgw-api-host <host>
+      ceph dashboard set-rgw-api-port 8080
+      # Putting option incase we need to enable/disable https
+      # ceph dashboard set-rgw-api-scheme <scheme>  # http or https
+      # Need to investigate the below
+      # ceph dashboard set-rgw-api-admin-resource <admin_resource>
+      echo "Disable ssl_verify until we are on signed certs"
+      ceph dashboard set-rgw-api-ssl-verify False
+      # Add checks for verifying the dashboard is up and functional
+    else
+      echo "passwords did not match, please re-run the install"
+    fi
 }
