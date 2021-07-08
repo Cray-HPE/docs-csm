db3fcd2be362bc65e52fb0a3c005097e240d97a5
Dat Le <dle@cray.com>
2021-07-08 16:32:37 -0500

    fix error handling if psp did not exist and modify perms

--- upgrade/0.9/csm-0.9.4/scripts/check-unbound-psp.sh
+++ upgrade/0.9/csm-0.9.4/scripts/check-unbound-psp.sh
@@ -7,8 +7,9 @@ set -exo pipefail
 # Check for manually create unbound PSP that is not managed by helm
 
 echo "Checking for manually created cray-dns-unbound-psp"
-unbound_psp="$(kubectl get ClusterRoleBinding -n services cray-dns-unbound-psp -o yaml |grep helm |wc -l)"||true
-if [[ "$unbound_psp" -eq "0" ]]; then
+unbound_psp_exist="$(kubectl get ClusterRoleBinding -n services |grep cray-dns-unbound-psp|wc -l)"||true
+unbound_psp_helm_check="$(kubectl get ClusterRoleBinding -n services cray-dns-unbound-psp -o yaml |grep helm |wc -l)"||true
+if [[ "$unbound_psp_helm_check" -eq "0" ]] && [[ "$unbound_psp_exist" -eq "1" ]]; then
     echo "Found ClusterRoleBinding cray-dns-unbound-psp NOT managed by helm"
     kubectl delete ClusterRoleBinding -n services cray-dns-unbound-psp
     echo "Delete ClusterRoleBinding cray-dns-unbound-psp"
