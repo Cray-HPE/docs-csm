40a759709bdf63152090afed2d81aa6ab0c5e9c0
Jacob Salmela <jacob.salmela@hpe.com>
2021-07-20 15:29:10 -0500

    CASMINST-2689 run workaround fix for xml kernel/initrd as a prerequisite to upgrading to 1.0
    Signed-off-by: Jacob Salmela <jacob.salmela@hpe.com>

--- upgrade/1.0/scripts/upgrade/prerequisites.sh
+++ upgrade/1.0/scripts/upgrade/prerequisites.sh
@@ -39,37 +39,31 @@ if [[ -z ${CSM_RELEASE} ]]; then
     exit 1
 fi
 
-# Apply WAR for CASMINST-1612, just in case
+# Apply WAR for CASMINST-2689, just in case
 if [[ $(hostname) == "ncn-m001" ]]; then
-    echo "Opening and refreshing fallback artifacts on the NCNs.."
-    pdsh -b -S -w $(grep -oP 'ncn-\w\d+' /etc/hosts | sort -u |  tr -t '\n' ',') '
-    set -x
-    RC=1
-    if ! mkdir -pv /tmp/mount ; then
-        echo >&2 "ERROR: mkdir command failed"
-        exit 1
-    elif ! mount -vL BOOTRAID /tmp/mount/ ; then
-        echo >&2 "ERROR: mount command failed"
-        exit 1
-    elif [ ! -d /tmp/mount/boot ]; then
-        echo >&2 "ERROR: BOOTRAID is missing grub and more"
-    elif ! cp -pv /run/initramfs/live/LiveOS/initrd* /tmp/mount/boot/initrd.img.xz ; then
-        # We assume there should only be one initrd file in the LiveOS directory. If there
-        # are multiple, the copy will fail, which is what we want.
-        echo >&2 "ERROR: cp command failed"
-    elif ! cp -pv /run/initramfs/live/LiveOS/kernel /tmp/mount/boot/ ; then
-        echo >&2 "ERROR: cp command failed"
-    else
-        # Both copies succeeded
-        RC=0
-    fi
-    umount -v /tmp/mount || echo >&2 "WARNING: umount command failed"
-    exit $RC'
+  echo "Opening and refreshing fallback artifacts on the NCNs.."
+
+  # Run the workaround on ncn-m001 or exit if it's not there
+  if [[ -f /opt/cray/csm/workarounds/livecd-post-reboot/CASMINST-2689/CASMINST-2689.sh ]]; then
+    /opt/cray/csm/workarounds/livecd-post-reboot/CASMINST-2689/CASMINST-2689.sh
+  else
+    echo "Please install the latest csm-install-workarounds"
+    exit 1
+  fi
+
+  # Copy it to the other NCNs
+  for i in $(grep -oP 'ncn-\w\d+' /etc/hosts | sort -u |  tr -t '\n' ' ');
+  do
+    scp -r /opt/cray/csm/workarounds/livecd-post-reboot/CASMINST-2689 $i:/opt/cray/csm/workarounds/livecd-post-reboot/
+  done
+
+  # Run the workaround on all the NCNs
+  pdsh -b -S -w $(grep -oP 'ncn-\w\d+' /etc/hosts | sort -u |  tr -t '\n' ',') '/opt/cray/csm/workarounds/livecd-post-reboot/CASMINST-2689/CASMINST-2689.sh'
 fi
 
 if [[ -z ${TARBALL_FILE} ]]; then
-    # Download tarball from internet 
-    
+    # Download tarball from internet
+
     if [[ -z ${ENDPOINT} ]]; then
         # default endpoint to internal artifactory
         ENDPOINT=https://arti.dev.cray.com/artifactory/shasta-distribution-unstable-local/csm/
