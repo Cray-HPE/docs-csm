4b002615f27ba21b251cda7e6408a2bb2b2cfd74
Lindsay Eliasen <lindsay.eliasen@hpe.com>
2021-08-02 16:45:15 -0500

    CASMINST-2793
    Docs change. Added troubleshooting procedure for rgw health check failing.

---
+++ operations/utility_storage/Troubleshoot_RGW_Health_Check_Fail.sh
@@ -0,0 +1,88 @@
+# Troubleshoot if RGW Health Check Fails
+
+Use this procedure to determine why the "rgw health check" failed and what needs to be fixed.
+
+## Prerequisites
+
+Anything to write here? Which node to run the commands on??
+
+## Procedure
+
+In the key-value pair outputted by the goss test "Expected \< int \>: x", look at the value of 'x' (possible values are 1,2,3,4,5). Based on this, navigate to corresponding numbered item below for troubleshooting this issue.
+
+Optional: Manually run the rgw health check script to see descriptive output.
+
+```bash
+ncn-m001# GOSS_BASE=/opt/cray/tests/install/ncn /opt/cray/tests/install/ncn/scripts/rgw_health_check.sh
+```
+
+
+1. A value of 1 is returned if unable to connect to rgw-vip. This happens if any of the following three commands fail.
+
+    ```bash
+    ncn-m001# curl -i -s -S -k https://rgw-vip.nmn
+    ncn-m001# curl -i -s -S http://rgw-vip.nmn
+    ncn-m001# curl -i -s -S http://rgw-vip.hmn
+    ```
+
+    To fix this, Do THIS
+
+1. A value of 2 is returned if a storage node is not able to be reached. In this case, run the "rgw_health_check.sh" described in the optional step above. Find which storage node(s) is not able to be reached, and run the following checks on that node.
+
+    - Check HAProxy is running on the node.
+        
+        ```bash
+        ncn-s001# systemctl status haproxy
+        ```
+        If HAProxy is not running, restart it and check the status again.
+        ```bash
+        ncn-s001# systemctl restart haproxy
+        ncn-s001# systemctl status haproxy
+        ```
+
+    - Check keepalived is running on the node.
+
+        ```bash
+        ncn-s001# systemctl status keepalived.service
+        ```
+        If keepalived is not running, restart it and check the status again.
+        ```bash
+        ncn-s001# systemctl restart keepalived.service
+        ncn-s001# systemctl status keepalived.service
+        ```
+
+    - Check ceph-rgw daemon is running.
+        ```bash
+        ncn-s001# ceph -s | grep rgw
+        ```
+        If ceph-rgw daemon is not running on 3 storage nodes (CRAIG CHECK should just run on 3??), restart the deamon and watch it come up within a few seconds.
+
+        ```bash
+        ncn-s001# ceph orch ps | grep rgw           #use this to wach the daemon start
+        ncn-s001# ceph orch damon restart <name>
+        ```
+
+
+1. A value of 3 is returned if a 'craysys' command fails. This implies 'cloud-init' is not healthy. Run the command below to determine the health.
+
+    ```bash
+    ncn-s001# cloud-init query -a
+    ```
+
+    If the command above fails, reinitialize 'cloud-init' using the following command.
+
+    ```bash
+    ncn-s001# cloud-init init
+    ```
+
+
+1. If a value of 4 or 5 is returned, then rgw-vip and the storage nodes are reachable. Thee error occured when attempting to create a bucket, upload an object to a bucket, or download an object from a bucket. This implies ceph may be unhealthy. Check ceph status with the following command.
+
+    ```bash
+    ncn-s001# ceph -s
+    ```
+
+    If ceph reports any status other than "HEALTH_OK", go to [Utility Storage](Utility_Storage.md) for general ceph troubleshooting. 
+
+1. Refer to point 4. 
+
