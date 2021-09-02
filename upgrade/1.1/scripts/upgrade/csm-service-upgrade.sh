a7ff484659bba8c53d69a8a1dd9907e27856f033
Kevin Burns <kevin.burns@hpe.com>
2021-09-02 14:03:03 -0500

    CASMPET-3997 Add storage workload spire entry

--- upgrade/1.1/scripts/upgrade/csm-service-upgrade.sh
+++ upgrade/1.1/scripts/upgrade/csm-service-upgrade.sh
@@ -9,11 +9,21 @@ trap 'err_report' ERR
 
 . /etc/cray/upgrade/csm/myenv
 
+state_name="POST_CSM_UPDATE_SPIRE_ENTRIES"
+state_recorded=$(is_state_recorded "${state_name}" $(hostname))
+if [[ $state_recorded == "0" ]]; then
+    echo "====> ${state_name} ..."
+    /usr/share/doc/csm/upgrade/1.1/scripts/upgrade/update-spire-entries.sh
+    record_state ${state_name} $(hostname)
+else
+    echo "====> ${state_name} has been completed"
+fi
+
 state_name="POST_CSM_JOIN_SPIRE_ON_STORAGE"
 state_recorded=$(is_state_recorded "${state_name}" $(hostname))
 if [[ $state_recorded == "0" ]]; then
     echo "====> ${state_name} ..."
-    /usr/share/doc/csm/upgrade/1.0/scripts/upgrade/join-spire-on-storage.sh
+    /usr/share/doc/csm/upgrade/1.1/scripts/upgrade/join-spire-on-storage.sh
     record_state ${state_name} $(hostname)
 else
     echo "====> ${state_name} has been completed"
