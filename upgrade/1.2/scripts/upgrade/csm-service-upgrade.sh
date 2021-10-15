ffd2e9194d5c2a3422962ac05a336ac276f4531d
Brad Klein <bklein@cray.com>
2021-10-15 12:59:06 -0600

    Several 1.2 specific upgrade changes
    * Remove pod priority script
    * Apply weave/multus version upgrade after NCNs upgraded

--- upgrade/1.2/scripts/upgrade/csm-service-upgrade.sh
+++ upgrade/1.2/scripts/upgrade/csm-service-upgrade.sh
@@ -77,16 +77,6 @@ else
     echo "====> ${state_name} has been completed"
 fi
 
-state_name="POST_CSM_UPGRADE_APPLY_POD_PRIORITY"
-state_recorded=$(is_state_recorded "${state_name}" $(hostname))
-if [[ $state_recorded == "0" ]]; then
-    echo "====> ${state_name} ..."
-    /usr/share/doc/csm/upgrade/1.2/scripts/upgrade/add_pod_priority.sh
-    record_state ${state_name} $(hostname)
-else
-    echo "====> ${state_name} has been completed"
-fi
-
 state_name="POST_CSM_UPDATE_SPIRE_ENTRIES"
 state_recorded=$(is_state_recorded "${state_name}" $(hostname))
 if [[ $state_recorded == "0" ]]; then
