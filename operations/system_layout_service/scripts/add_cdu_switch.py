b97609b48b634ed1512e904c9bba81e7d023c57b
Ryan Sjostrand <sjostrand@hpe.com>
2022-01-11 10:25:16 -0600

    Add support for CDU Switches in RiverCabinet, and add IPs if the CMN network is present. Which is a CSM 1.2 item.

--- operations/system_layout_service/scripts/add_cdu_switch.py
+++ operations/system_layout_service/scripts/add_cdu_switch.py
@@ -1,10 +1,34 @@
 #! /usr/bin/env python3
 
+# MIT License
+#
+# (C) Copyright [2022] Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
 import argparse
 import json
 import re
 import netaddr
-import shutil
+
+CDU_MGMT_SWITCH_XNAME_REGEX="^d([0-9]+)w([0-9]+)$"
+MGMT_HL_SWITCH_XNAME_REGEX="^x([0-9]{1,4})c([0-7])h([1-9][0-9]*)s([1-9])$"
 
 def find_subnet(sls_network, name):
     network_hardware_subnet = None
@@ -57,7 +81,7 @@ parser.add_argument("--brand", type=str, required=True, help="Switch brand", cho
 parser.add_argument("--alias", type=str, required=True, help="CDU Switch alias, ex: sw-cdu-003")
 args = parser.parse_args()
 
-if re.match("^d([0-9]+)w([0-9]+)$", args.cdu_switch) == None:
+if re.match(CDU_MGMT_SWITCH_XNAME_REGEX, args.cdu_switch) == None and re.match(MGMT_HL_SWITCH_XNAME_REGEX, args.cdu_switch) == None:
     print("Invalid CDU Switch xname provided: ", args.cdu_switch)
     exit(1)
 
@@ -85,17 +109,33 @@ allNetworks = sls_state["Networks"]
 #
 # Hardware
 #
-cdu_switch = {
-    "Parent": re.compile("w([0-9]+)$").sub("", args.cdu_switch),
-    "Xname": args.cdu_switch,
-    "Type": "comptype_cdu_mgmt_switch",
-    "Class": "Mountain",
-    "TypeString": "CDUMgmtSwitch",
-    "ExtraProperties": {
-        "Brand": args.brand,
-        "Aliases": [args.alias]
+cdu_switch = None
+if re.match(CDU_MGMT_SWITCH_XNAME_REGEX, args.cdu_switch) != None:
+    # CDU Switch located within a CDU
+    cdu_switch = {
+        "Parent": re.compile("w([0-9]+)$").sub("", args.cdu_switch),
+        "Xname": args.cdu_switch,
+        "Type": "comptype_cdu_mgmt_switch",
+        "Class": "Mountain",
+        "TypeString": "CDUMgmtSwitch",
+        "ExtraProperties": {
+            "Brand": args.brand,
+            "Aliases": [args.alias]
+        }
     }
-}
+else:
+    # CDU Switch located within a River cabinet
+    cdu_switch = {
+        "Parent": re.compile("s([1-9])$").sub("", args.cdu_switch),
+        "Xname": args.cdu_switch,
+        "Type": "comptype_hl_switch",
+        "Class": "River",
+        "TypeString": "MgmtHLSwitch",
+        "ExtraProperties": {
+            "Brand": args.brand,
+            "Aliases": [args.alias]
+        }
+    }
 
 # Verify the CDU switch has a unique xname
 if args.cdu_switch in allHardware:
@@ -106,7 +146,10 @@ if args.cdu_switch in allHardware:
 for xname in allHardware:
     hardware = allHardware[xname]
 
-    if hardware["Type"] != "comptype_cdu_mgmt_switch" or "ExtraProperties" not in hardware:
+    if hardware["Type"] != "comptype_cdu_mgmt_switch" and hardware["Type"] != "comptype_hl_switch":
+        continue
+    
+    if "ExtraProperties" not in hardware:
         continue
     
     if "Aliases" not in hardware["ExtraProperties"]:
@@ -127,14 +170,20 @@ print("================================")
 print("CDU Switch Network Configuration")
 print("================================")
 
+network_names = ["HMN", "NMN", "MTL"]
+if "CMN" in allNetworks:
+    network_names.append("CMN")
+
 ips = {}
-for network_name in ["HMN", "NMN", "MTL"]:
+for network_name in network_names:
     ips[network_name] = add_cdu_ip_reservation(allNetworks[network_name], args.cdu_switch, args.alias)
 
 print()
 print("HMN IP: {}".format(ips["HMN"]))
 print("NMN IP: {}".format(ips["NMN"]))
 print("MTL IP: {}".format(ips["MTL"]))
+if "CMN" in ips:
+    print("CMN IP: {}".format(ips["CMN"]))
 print()
 
 # Write out the updated SLS dump
