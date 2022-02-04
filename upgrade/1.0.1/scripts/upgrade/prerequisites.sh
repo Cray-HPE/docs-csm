c28353daf42321363c820d5408a5fd0fa54ad37e
Mitch Harding <mitchell.harding@hpe.com>
2022-02-04 16:55:48 -0500

    CASMINST-3965: Correctly record cps nodes before csm upgrade

--- upgrade/1.0.1/scripts/upgrade/prerequisites.sh
+++ upgrade/1.0.1/scripts/upgrade/prerequisites.sh
@@ -570,9 +570,17 @@ else
     echo "====> ${state_name} has been completed"
 fi
 
-# Take cps deployment snapshot
-cps_deployment_snapshot=$(cray cps deployment list --format json | jq -r '.[] | .node' || true)
-echo $cps_deployment_snapshot > /etc/cray/upgrade/csm/${CSM_RELEASE}/cp.deployment.snapshot
+# Take cps deployment snapshot (if cps installed)
+set +e
+trap - ERR
+kubectl get pod -n services | grep -q cray-cps
+if [ "$?" -eq 0 ]; then
+  cps_deployment_snapshot=$(cray cps deployment list --format json | jq -r \
+    '.[] | select(."podname" != "NA" and ."podname" != "") | .node' || true)
+  echo $cps_deployment_snapshot > /etc/cray/upgrade/csm/${CSM_RELEASE}/cp.deployment.snapshot
+fi
+trap 'err_report' ERR
+set -e
 
 # Alert the user of action to take for cleanup
 if [[ ${#UNMOUNTS[@]} -ne 0 ]]; then
