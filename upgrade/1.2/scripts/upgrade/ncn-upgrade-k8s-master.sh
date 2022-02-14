28bd372695fa6131a3536ce796499652b73f2b1a
Brad Klein <bklein@cray.com>
2022-02-14 10:16:57 -0700

    Add step to reconfig m001 kube-api service issuer before worker upgrades

--- upgrade/1.2/scripts/upgrade/ncn-upgrade-k8s-master.sh
+++ upgrade/1.2/scripts/upgrade/ncn-upgrade-k8s-master.sh
@@ -187,6 +187,20 @@ else
     echo "====> ${state_name} has been completed"
 fi
 
+if [[ ${upgrade_ncn} != "ncn-m001" ]]; then
+   state_name="UPDATE_M001_KUBEAPI_SERVICE_ISSUER"
+   state_recorded=$(is_state_recorded "${state_name}" ncn-m001
+   if [[ $state_recorded == "0" ]]; then
+      echo "====> ${state_name} ..."
+
+      /usr/share/doc/csm/upgrade/1.2/scripts/k8s/update_kubeapi_service_issuer.sh ncn-m001
+
+      record_state "${state_name}" ncn-m001
+   else
+      echo "====> ${state_name} has been completed"
+   fi
+fi
+
 cat <<EOF
 
 NOTE:
