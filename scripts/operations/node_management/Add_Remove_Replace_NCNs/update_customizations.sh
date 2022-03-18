08f63911f8b0484ee7ecad6d0a7bf60ad4b0293e
Ryan Sjostrand <sjostrand@hpe.com>
2022-03-18 10:14:12 -0500

    Add initial update_customizations.sh script

---
+++ scripts/operations/node_management/Add_Remove_Replace_NCNs/update_customizations.sh
@@ -0,0 +1,70 @@
+#! /usr/bin/env bash
+# MIT License
+#
+# (C) Copyright [2022] Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+set -eu
+set -o pipefail
+
+if [[ -z "$TOKEN" ]]; then
+    echo "Error: TOKEN enviroment variable not set"
+    exit 1
+fi
+
+# Create tempory working directory
+tmp_dir=$(mktemp -d -t ncn-update-customizations-XXXX)
+echo "Working directory $tmp_dir"
+pushd "$tmp_dir"
+
+# Grab customizations.yaml
+kubectl -n loftsman get secret site-init -o jsonpath='{.data.customizations\.yaml}' | base64 -d - > customizations.yaml
+cp customizations.yaml customizations.orginal.yaml
+
+# Update Storage NCN IPs
+storage_ips=$(curl --fail -k -H "Authorization: Bearer ${TOKEN}" https://api-gw-service-nmn.local/apis/sls/v1/networks/NMN | 
+    jq '[.ExtraProperties.Subnets[] |  select(.Name == "bootstrap_dhcp").IPReservations[] | select(.Name | startswith("ncn-s")).IPAddress] | sort')
+
+yq merge -xP -i customizations.yaml <(echo "$storage_ips" | yq prefix -P - spec.network.netstaticips.nmn_ncn_storage)
+
+# Update Master NCN IPs
+storage_ips=$(curl --fail -k -H "Authorization: Bearer ${TOKEN}" https://api-gw-service-nmn.local/apis/sls/v1/networks/NMN | 
+    jq '[.ExtraProperties.Subnets[] |  select(.Name == "bootstrap_dhcp").IPReservations[] | select(.Name | startswith("ncn-m")).IPAddress] | sort')
+
+yq merge -xP -i customizations.yaml <(echo "$storage_ips" | yq prefix -P - spec.network.netstaticips.nmn_ncn_masters)
+
+echo "Orginal customizations.yaml is located at $tmp_dir/customizations.orginal.yaml"
+echo "Updated customizations.yaml is located at $tmp_dir/customizations.yaml"
+
+# Notes - Next steps
+# Compare cusotmizations.yaml
+# yq r customizations.orginal.yaml -P   > customizations.orginal.yaml.pretty
+# diff customizations.orginal.yaml.pretty customizations.yaml
+# 
+# ncn-m001:/tmp/ncn-update-customizations-8qfC # diff customizations.orginal.yaml.pretty customizations.yaml
+# 24a25
+# >         - 10.252.1.14
+
+# Update `site-init` sealed secret in `loftsman` namespace:
+
+# ```bash
+# ncn-m001# kubectl delete secret -n loftsman site-init
+# ncn-m001# kubectl create secret -n loftsman generic site-init --from-file=customizations.yaml
+# ```
+
