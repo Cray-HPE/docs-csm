b85ddcccf1cffbbee7118b7155593246339a970f
cdelatte-hpe <77167617+cdelatte-hpe@users.noreply.github.com>
2022-03-30 12:35:39 -0400

    CASMINST-4290 - Add wait for rgw to be started on upgraded node
    This will allow the rgw process to start and us not enter a race
    condition where the goss test is checking for a process that does
    not get spawned until after the cluster is healthy

--- upgrade/1.2/scripts/upgrade/ncn-upgrade-ceph-nodes.sh
+++ upgrade/1.2/scripts/upgrade/ncn-upgrade-ceph-nodes.sh
@@ -158,6 +158,19 @@ fi
 . /usr/share/doc/csm/upgrade/1.2/scripts/ceph/lib/ceph-health.sh
 wait_for_health_ok
 
+# Wait for rgw to start before executing goss tests
+target_ncn=ncn-s001
+rgw_counter=0
+until [[ $(ceph orch ps --daemon_type rgw ${target_ncn} --format json-pretty|jq -r '.[].status_desc') == "running" ]]
+do
+  sleep 30
+  let rgw_counter+=1
+  if rgw_counter -gt 10
+  then
+    exit 1
+  fi
+done
+
 if [[ ${target_ncn} == "ncn-s001" ]]; then
     state_name="POST_CEPH_IMAGE_UPGRADE_BUCKETS"
     state_recorded=$(is_state_recorded "${state_name}" ${target_ncn})
