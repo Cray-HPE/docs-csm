b934b7155a53e27a610d352e3a54457f107bc762
Mitch Harding <mitchell.harding@hpe.com>
2022-04-06 15:31:15 -0400

    CASMINST-4408: Linting

--- upgrade/1.2/scripts/k8s/failover-leader.sh
+++ upgrade/1.2/scripts/k8s/failover-leader.sh
@@ -46,7 +46,7 @@ do
         new_master="$podbasename-1"
     fi
 
-    # An http 200 response code is returned only if the pod is the master of the postgres cluster
+    # An HTTP 200 response code is returned only if the pod is the master of the postgres cluster
     master="$(kubectl exec $podname -c postgres -n $namespace -- curl -sL -w "%{http_code}" -I -X GET http://localhost:8008/master -o /dev/null)"
 
     if [ "$master" == "200" ]
