001aeaa6e7d05aada1970774b088a136e4b83c43
Prudvi Danda <78571901+prudvi-hpe@users.noreply.github.com>
2022-05-10 10:02:19 -0700

    Update update-customizations.sh
    SSHOTMGP-5040 Export system domain name to Fabric Manager container

--- upgrade/1.2/scripts/upgrade/util/update-customizations.sh
+++ upgrade/1.2/scripts/upgrade/util/update-customizations.sh
@@ -364,6 +364,8 @@ yq w -i "$c" 'spec.kubernetes.services.sma-rsyslog-aggregator-udp.rsyslogAggrega
 yq w -i "$c" 'spec.kubernetes.services.sma-kibana.externalAuthority' 'sma-kibana.cmn.{{ network.dns.external }}'
 yq w -i "$c" 'spec.kubernetes.services.sma-grafana.externalAuthority' 'sma-grafana.cmn.{{ network.dns.external }}'
 yq w -i "$c" 'spec.kubernetes.services.gatekeeper-policy-manager.gatekeeper-policy-manager.externalAuthority' 'opa-gpm.cmn.{{ network.dns.external }}'
+yq w -i "$c" 'spec.kubernetes.services.slingshot-fabric-manager.base_domain' "{{ network.dns.external }}"
+
 
 # cray-opa changes
 yq d -i "$c" 'spec.kubernetes.services.cray-opa.jwtValidation'
