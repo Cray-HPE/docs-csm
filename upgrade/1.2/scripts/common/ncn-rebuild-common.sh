217b5e6a561a5cdf0b67dde7384f06f35022b7ae
Mitch Harding <77123337+mharding-hpe@users.noreply.github.com>
2022-06-13 13:33:29 -0400

    Update ncn-rebuild-common.sh
    Correct shell check errors (rather than just disabling shell check)

--- upgrade/1.2/scripts/common/ncn-rebuild-common.sh
+++ upgrade/1.2/scripts/common/ncn-rebuild-common.sh
@@ -166,8 +166,7 @@ fi
     echo "mgmt IP/Host: ${target_ncn_mgmt_host}"
 
     # retrieve IPMI username/password from vault
-    #shellcheck disable=SC1083
-    VAULT_TOKEN=$(kubectl get secrets cray-vault-unseal-keys -n vault -o jsonpath={.data.vault-root} | base64 -d)
+    VAULT_TOKEN=$(kubectl get secrets cray-vault-unseal-keys -n vault -o jsonpath='{.data.vault-root}' | base64 -d)
     # Make sure we got a vault token
     [[ -n ${VAULT_TOKEN} ]]
 
@@ -184,11 +183,11 @@ fi
             jq -r '.data.Username')
         # If we are not able to get the username, no need to try and get the password.
         [[ -n ${IPMI_USERNAME} ]] || continue
-        #shellcheck disable=SC2155
-        export IPMI_PASSWORD=$(kubectl exec -it -n vault -c vault ${VAULT_POD} -- sh -c \
+        IPMI_PASSWORD=$(kubectl exec -it -n vault -c vault ${VAULT_POD} -- sh -c \
             "export VAULT_ADDR=http://localhost:8200; export VAULT_TOKEN=`echo $VAULT_TOKEN`; \
             vault kv get -format=json secret/hms-creds/$TARGET_MGMT_XNAME" | 
             jq -r '.data.Password')
+        export IPMI_PASSWORD
         break
     done
     # Make sure we found a pod that worked
