7f444123335c3dda0e3a9c3ef091805f349cff7d
Di <di.zou@hpe.com>
2022-06-20 09:18:45 -0400

    fix shell check

--- upgrade/1.2.6/scripts/post-csm-services-upgrade.sh
+++ upgrade/1.2.6/scripts/post-csm-services-upgrade.sh
@@ -26,10 +26,10 @@ set -eu
 
 argoUrl=$(kubectl get VirtualService/cray-argo -n argo -o json | jq -r '.spec.hosts[0]')
 
-export TOKEN=$(curl -k -s -S -d grant_type=password \
-   -d client_id=`kubectl get secrets keycloak-master-admin-auth -o jsonpath='{.data.client-id}' -n services | base64 -d` \
-   -d username=`kubectl get secrets keycloak-master-admin-auth -o jsonpath='{.data.user}' -n services | base64 -d` \
-   -d password=`kubectl get secrets keycloak-master-admin-auth -o jsonpath='{.data.password}' -n services | base64 -d` \
+TOKEN=$(curl -k -s -S -d grant_type=password \
+   -d client_id="$(kubectl get secrets keycloak-master-admin-auth -o jsonpath='{.data.client-id}' -n services | base64 -d)" \
+   -d username="$(kubectl get secrets keycloak-master-admin-auth -o jsonpath='{.data.user}' -n services | base64 -d)" \
+   -d password="$(kubectl get secrets keycloak-master-admin-auth -o jsonpath='{.data.password}' -n services | base64 -d)" \
    https://api-gw-service-nmn.local/keycloak/realms/master/protocol/openid-connect/token | jq -r '.access_token')
 
 foundArgoInRedirectUris=$(curl -s -k -H "Authorization: Bearer ${TOKEN}" "https://api-gw-service-nmn.local/keycloak/admin/realms/shasta/clients" | jq '.[] | select(.clientId=="oauth2-proxy-customer-management") | .redirectUris' | grep "https://argo.cmn" | wc -l)
@@ -43,7 +43,7 @@ id=$(curl -s -k -H "Authorization: Bearer ${TOKEN}" "https://api-gw-service-nmn.
 curl -s -k -H "Authorization: Bearer ${TOKEN}" "https://api-gw-service-nmn.local/keycloak/admin/realms/shasta/clients/${id}" > client.json
 if [[ ${foundArgoInRedirectUris} -ge 1 ]];then
     echo "Argo URL is set in RedirectUris"
-    echo ${redirectUris}
+    echo "${redirectUris}"
 else
     echo
     cat client.json | jq ".redirectUris[.redirectUris | length] |= . + \"https://$argoUrl/oauth/callback\""  | tee client.json
@@ -51,7 +51,7 @@ fi
 
 if [[ ${foundArgoInWebOrigins} -ge 1 ]];then
     echo "Argo URL is set in WebOrigins"
-    echo ${webOrigins}
+    echo "${webOrigins}"
 else
     echo
     cat client.json | jq ".webOrigins[.webOrigins | length] |= . + \"https://$argoUrl\"" | tee client.json
