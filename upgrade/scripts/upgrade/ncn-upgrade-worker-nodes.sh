2bb58541bd5537cefd611bb03f81c8c240965c1f
Di <di.zou@hpe.com>
2022-07-21 11:01:28 -0400

    rewrite worker upgrade script

--- upgrade/scripts/upgrade/ncn-upgrade-worker-nodes.sh
+++ upgrade/scripts/upgrade/ncn-upgrade-worker-nodes.sh
@@ -28,68 +28,215 @@ basedir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 . ${basedir}/../common/upgrade-state.sh
 trap 'err_report' ERR
 
-target_ncns=$1
-IFS=', ' read -r -a array <<< "$target_ncns"
-jsonArray=$(jq -r --compact-output --null-input '$ARGS.positional' --args -- "${array[@]}")
+#global vars
+dryRun=false
+baseUrl="https://api-gw-service-nmn.local"
+retry=false
+force=false
 
-${basedir}/../../../workflows/scripts/upload-worker-rebuild-templates.sh
+function usage() {
+    echo "CSM ncn worker upgrade script"
+    echo
+    echo "Syntax: /usr/share/doc/csm/upgrade/scripts/upgrade/ncn-upgrade-worker-nodes.sh [COMMA_SEPARATED_NCN_HOSTNAMES] [-f|--force|--retry|--base-url|--dry-run]"
+    echo "options:"
+    echo "--retry        Send RETRY request instead of CREATE if there is a failed worker rebuild/upgrade workflow already  (default: ${retry})"
+    echo "-f|--force     Remove failed worker rebuild/upgrade workflow and create a new one  (default: ${force})"
+    echo "--base-url     Specify base url (default: ${baseUrl})"
+    echo "--dry-run      Print out steps of workflow instead of running steps (default: ${dryRun})"
+    echo
+    echo "*COMMA_SEPARATED_NCN_HOSTNAMES"
+    echo "  example 1) ncn-w001"
+    echo "  example 2) ncn-w001,ncn-w002,ncn-w003"
+    echo
+}
 
-if [[ -z "${SW_PASSWORD}" ]]; then
-  read -r -s -p "Switch password:" SW_PASSWORD
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --retry)
+        retry=true
+        shift # past argument
+        ;;
+    -f|--force)
+        force=true
+        shift # past argument
+        ;;
+    --base-url)
+        baseUrl="$2"
+        shift # past argument
+        shift # past value
+        ;;
+    --dry-run)
+        dryRun=true
+        shift # past argument
+        ;;
+    ncn-w[0-9][0-9][0-9]*)
+        target_ncns=$1
+        shift # past argument
+        IFS=', ' read -r -a array <<< "$target_ncns"
+        jsonArray=$(jq -r --compact-output --null-input '$ARGS.positional' --args -- "${array[@]}")
+        ;;
+    *)
+        echo 
+        echo "Unknown option $1" 
+        usage
+        exit 1
+        ;;
+  esac
+done
+
+if $retry && $force; then
+    echo "WARNING: RETRY is ignored when FORCE is set"
+    retry=false
 fi
 
-cat << EOF > data.json
+function uploadWorkflowTemplates() {
+    "${basedir}"/../../../workflows/scripts/upload-worker-rebuild-templates.sh
+}
+
+function createWorkflowPayload() {
+    # ask for switch password if it's not set
+    if [[ -z "${SW_ADMIN_PASSWORD}" ]]; then
+    read -r -s -p "Switch password:" SW_ADMIN_PASSWORD
+    fi
+    echo
+    cat << EOF
 {
-  "dryRun": false,
+  "dryRun": ${dryRun},
   "hosts": ${jsonArray},
-  "switchPassword": "${SW_PASSWORD}"
+  "switchPassword": "${SW_ADMIN_PASSWORD}"
 }
 EOF
-# shellcheck disable=SC2155,SC2046
-export TOKEN=$(curl -k -s -S -d grant_type=client_credentials \
-   -d client_id=admin-client \
-   -d client_secret=`kubectl get secrets admin-client-auth -o jsonpath='{.data.client-secret}' | base64 -d` \
-   https://api-gw-service-nmn.local/keycloak/realms/shasta/protocol/openid-connect/token | jq -r '.access_token')
+}
 
-response=$(curl -sk -XPOST -H "Authorization: Bearer ${TOKEN}" -H 'Content-Type: application/json' -d @data.json "https://api-gw-service-nmn.local/apis/nls/v1/ncns/rebuild")
+function getToken() {
+    # shellcheck disable=SC2155,SC2046
+    curl -k -s -S -d grant_type=client_credentials \
+        -d client_id=admin-client \
+        -d client_secret=`kubectl get secrets admin-client-auth -o jsonpath='{.data.client-secret}' | base64 -d` \
+        "${baseUrl}"/keycloak/realms/shasta/protocol/openid-connect/token | jq -r '.access_token'
+}
 
-rm -rf data.json
+function printCmdArgs() {
+    echo "Inputs: "
+    echo "============================================================"
+    echo "Target Ncn(s):    ${jsonArray}"
+    echo "Base URL:         ${baseUrl}"
+    echo "Retry:            ${retry}"
+    echo "Dry Run:          ${dryRun}"
+    echo "Force:            ${force}"
+    echo "============================================================="
+}
 
-if echo "${response}" | grep "message"; then
-    echo
-    echo "${response}" | jq -r '.message'
+function getUnsucceededWorkerRebuildWorkflows() {
+    res_file=$(mktemp)
+    local labelSelector="node-type=worker,workflows.argoproj.io/phase!=Succeeded"
+    http_code=$(curl -s -o "${res_file}" -w "%{http_code}" -k -XGET -H "Authorization: Bearer $(getToken)" "${baseUrl}/apis/nls/v1/workflows?labelSelector=${labelSelector}")
+    if [[ ${http_code} -ne 200 ]]; then
+        echo "Request Failed, Response code: ${http_code}"
+        cat "${res_file}"
+        exit 1
+    fi
+    jq -r ".[]? | .name?" < "${res_file}"
+}
+
+function createRebuildWorkflow() {
+    res_file=$(mktemp)
+    http_code=$(curl -s -o "${res_file}" -w "%{http_code}" -k -XPOST -H "Authorization: Bearer $(getToken)" -H 'Content-Type: application/json' -d "$(createWorkflowPayload)" "${baseUrl}/apis/nls/v1/ncns/rebuild")
+    if [[ ${http_code} -ne 200 ]]; then
+        echo "Request Failed, Response code: ${http_code}"
+        cat "${res_file}"
+        exit 1
+    fi
+    local workflow
+    workflow=$(grep -o 'ncn-lifecycle-rebuild-[a-z0-9]*' < "${res_file}" )
+    echo "${workflow}"
+}
+
+function deleteRebuildWorkflow() {
+    res_file=$(mktemp)
+    http_code=$(curl -s -o "${res_file}" -w "%{http_code}" -k -XDELETE -H "Authorization: Bearer $(getToken)" "${baseUrl}/apis/nls/v1/workflows/${1}")
+    if [[ ${http_code} -ne 200 ]]; then
+        echo "Request Failed, Response code: ${http_code}"
+        cat "${res_file}"
+        exit 1
+    fi
+}
+
+function retryRebuildWorkflow() {
+    res_file=$(mktemp)
+    http_code=$(curl -s -o "${res_file}" -w "%{http_code}" -k -XPUT -H "Authorization: Bearer $(getToken)" "${baseUrl}/apis/nls/v1/workflows/${1}/retry")
+    if [[ ${http_code} -ne 200 ]]; then
+        echo "Request Failed, Response code: ${http_code}"
+        cat "${res_file}"
+    fi
+}
+
+printCmdArgs
+unsucceededWorkflows=($(getUnsucceededWorkerRebuildWorkflows))
+numOfUnsucceededWorkflows="${#unsucceededWorkflows[*]}"
+
+if [[ ${numOfUnsucceededWorkflows} -gt 1 ]]; then
+    echo "ERROR: There are multiple unsucceeded worker rebuild workflows"
+    exit 1
 fi
 
-workflow=$(echo "${response}" | grep -o 'ncn-lifecycle-rebuild-[a-z0-9]*["|\.]')
-workflow=${workflow::-1}
+# dealing with FORCE
+# shellcheck disable=SC2046
+if $force && [ "${numOfUnsucceededWorkflows}" -eq 1 ]; then
+    workflow=$(echo "${unsucceededWorkflows[0]}" | grep -o 'ncn-lifecycle-rebuild-[a-z0-9]*')
+    echo "Delete workflow: ${workflow}"
+    deleteRebuildWorkflow "${workflow}"
+fi
 
-echo
-echo "Running workflow: ${workflow}"
+# dealing with RETRY
+# shellcheck disable=SC2046
+if $retry && [ "${numOfUnsucceededWorkflows}" -eq 1 ]; then
+    workflow=$(echo "${unsucceededWorkflows[0]}" | grep -o 'ncn-lifecycle-rebuild-[a-z0-9]*')
+    echo "Retry workflow: ${workflow}"
+    retryRebuildWorkflow "${workflow}"
+fi
 
+if [ "${numOfUnsucceededWorkflows}" -eq 0 ]; then
+    # create a new workflow
+    workflow=$(createRebuildWorkflow)
+    echo "Create workflow: ${workflow}"
+fi
+
+#sleep 20
+
+# poll
 while true; do
-    # TODO: limit to worker rebuild request
-    phase=$(curl -sk -XGET -H "Authorization: Bearer ${TOKEN}" "https://api-gw-service-nmn.local/apis/nls/v1/workflows" | \
-        jq -r ".[] | select(.name==\"${workflow}\") | .status.phase")
-    # skip null because workflow hasn't started yet
-    if [[ "${phase}" == "null" ]]; then
-        continue;
-    fi
+    labelSelector="node-type=worker"
+    res_file="$(mktemp)"
+    http_status=$(curl -s -o "${res_file}" -w "%{http_code}" -k -XGET -H "Authorization: Bearer $(getToken)" "${baseUrl}/apis/nls/v1/workflows?labelSelector=${labelSelector}")
+    
+    if [ "${http_status}" -eq 200 ]; then
+        phase=$(jq -r ".[] | select(.name==\"${workflow}\") | .status.phase" < "${res_file}")
+        # skip null because workflow hasn't started yet
+        if [[ "${phase}" == "null" ]]; then
+            continue;
+        fi
 
-    if [[ "${phase}" == "Succeeded" ]]; then
-        break;
-    fi
+        if [[ "${phase}" == "Succeeded" ]]; then
+            break;
+        fi
 
-    if [[ "${phase}" == "Failed" ]]; then
-        # TODO: get los/troubleshooting
-        break;
-    fi
+        if [[ "${phase}" == "Failed" ]]; then
+            # TODO: get los/troubleshooting
+            break;
+        fi
 
-    if [[ "${phase}" == "Error" ]]; then
-        echo "Workflow in Error state, Retry ..."
-        curl -sk -XPUT -H "Authorization: Bearer ${TOKEN}" "https://api-gw-service-nmn.local/apis/nls/v1/workflows/${workflow}/retry"
-        sleep 20
+        if [[ "${phase}" == "Error" ]]; then
+            echo "Workflow in Error state, Retry ..."
+            curl -sk -XPUT -H "Authorization: Bearer $(getToken)" "${baseUrl}/apis/nls/v1/workflows/${workflow}/retry"
+        fi
+        runningSteps=$(jq -jr ".[] | select(.name==\"${workflow}\") | .status.nodes[] | select(.type==\"Retry\")| select(.phase==\"Running\")  | .displayName + \"\n  \" " < "${res_file}")
+        succeededSteps=$(jq -jr ".[] | select(.name==\"${workflow}\") | .status.nodes[] | select(.type==\"Retry\")| select(.phase==\"Succeeded\")  | .displayName +\"\n  \" " < "${res_file}")
+        printf "\n%s\n" "Succeeded:"
+        echo "  ${succeededSteps}"
+        printf "%s\n" "${phase}:"
+        echo "  ${runningSteps}"
+        sleep 10
     fi
-    runningSteps=$(curl -sk -XGET -H "Authorization: Bearer ${TOKEN}" "https://api-gw-service-nmn.local/apis/nls/v1/workflows" |         jq -r ".[] | select(.name==\"${workflow}\") | .status.nodes[] | select(.type==\"Retry\")| select(.phase==\"Running\")  | .displayName")
-    echo "${phase}: ${runningSteps}"
-    sleep 10
 done
+
