43eb17e7ec29ae5aaafd890acf3df0253ce2655d
Lindsay Eliasen <lindsay.eliasen@hpe.com>
2022-07-29 17:08:07 -0500

    CASMISNT-4942  this resolves CASMINST-5143
    Resolves CASMINST-5143 by not printing to the terminal if terminal is not available. This is the case when calling script from argo workflow

--- upgrade/scripts/common/upgrade-state.sh
+++ upgrade/scripts/common/upgrade-state.sh
@@ -115,17 +115,35 @@ function err_report() {
         return 0
     fi
     
-    # force output to console regardless of redirection
-    echo >/dev/tty 
-    echo "[ERROR] - Unexpected errors, check logs: ${LOG_FILE}" >/dev/tty 
+    # check if /dev/tty is available, it is not available when using argo workflows
+    if sh -c ": >/dev/tty" >/dev/null 2>/dev/null; then
+        # /dev/tty is available and usable
+        # force output to console regardless of redirection
+        echo >/dev/tty 
+        echo "[ERROR] - Unexpected errors, check logs: ${LOG_FILE}" >/dev/tty
+    else
+        # /dev/tty is not available
+        echo
+        echo "[ERROR] - Unexpected errors, check logs: ${LOG_FILE}"
+    fi
+
     # avoid shell double trap
     NO_ERROR_TRAP=1
 }
 
 function ok_report() {
-    # force output to console regardless of redirection
-    echo >/dev/tty 
-    echo "[OK] - Successfully completed" >/dev/tty
+    # check if /dev/tty is available, it is not available when using argo workflows
+    if sh -c ": >/dev/tty" >/dev/null 2>/dev/null; then
+        # /dev/tty is available and usable
+        # force output to console regardless of redirection
+        echo >/dev/tty 
+        echo "[OK] - Successfully completed" >/dev/tty
+    else
+        # /dev/tty is not available
+        echo
+        echo "[OK] - Successfully completed"
+    fi
+
     # avoid shell double trap
     NO_ERROR_TRAP=1
 }
