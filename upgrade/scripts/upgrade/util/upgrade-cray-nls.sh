73390e68ea09a5f823078739c8c689dba48d5653
Di <di.zou@hpe.com>
2022-08-12 15:32:25 -0400

    CASMPET-5889 enable pipefail, add curl error handling
    (cherry picked from commit 928214b89df40c6ccd453ab284507cb46765dfbe)

--- upgrade/scripts/upgrade/util/upgrade-cray-nls.sh
+++ upgrade/scripts/upgrade/util/upgrade-cray-nls.sh
@@ -23,7 +23,7 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 #
 
-set -eu
+set -euo pipefail
 
 function deployNLS() {
     BUILDDIR="/tmp/build"
@@ -50,38 +50,57 @@ function patchKeycloak() {
     --data-urlencode username="$(kubectl get secrets keycloak-master-admin-auth -o jsonpath='{.data.user}' -n services | base64 -d)" \
     --data-urlencode password="$(kubectl get secrets keycloak-master-admin-auth -o jsonpath='{.data.password}' -n services | base64 -d)" \
     https://api-gw-service-nmn.local/keycloak/realms/master/protocol/openid-connect/token | jq -r '.access_token')
-    #shellcheck disable=SC2126
-    foundArgoInRedirectUris=$(curl -s -k -H "Authorization: Bearer ${TOKEN}" "https://api-gw-service-nmn.local/keycloak/admin/realms/shasta/clients" | jq '.[] | select(.clientId=="oauth2-proxy-customer-management") | .redirectUris' | grep "https://argo.cmn" | wc -l)
-    redirectUris=$(curl -s -k -H "Authorization: Bearer ${TOKEN}" "https://api-gw-service-nmn.local/keycloak/admin/realms/shasta/clients" | jq -r '.[] | select(.clientId=="oauth2-proxy-customer-management") | .redirectUris')
-    #shellcheck disable=SC2126
-    foundArgoInWebOrigins=$(curl -s -k -H "Authorization: Bearer ${TOKEN}" "https://api-gw-service-nmn.local/keycloak/admin/realms/shasta/clients" | jq '.[] | select(.clientId=="oauth2-proxy-customer-management") | .webOrigins' | grep "https://argo.cmn" | wc -l)
-    webOrigins=$(curl -s -k -H "Authorization: Bearer ${TOKEN}" "https://api-gw-service-nmn.local/keycloak/admin/realms/shasta/clients" | jq -r '.[] | select(.clientId=="oauth2-proxy-customer-management") | .webOrigins')
 
-    id=$(curl -s -k -H "Authorization: Bearer ${TOKEN}" "https://api-gw-service-nmn.local/keycloak/admin/realms/shasta/clients" | jq -r '.[] | select(.clientId=="oauth2-proxy-customer-management") | .id')
+    res_file=$(mktemp)
+    http_code=$(curl -s -o "${res_file}" -w "%{http_code}" -k -XGET -H "Authorization: Bearer ${TOKEN}" "https://api-gw-service-nmn.local/keycloak/admin/realms/shasta/clients")
+    if [[ ${http_code} -ne 200 ]]; then
+        echo "Request Failed, Response code: ${http_code}"
+        cat "${res_file}"
+        exit 1
+    fi
+    
+    id=$(jq -r '.[] | select(.clientId=="oauth2-proxy-customer-management") | .id'  < "${res_file}" )
+    echo "Get client id: ${id}"
 
-    curl -s -k -H "Authorization: Bearer ${TOKEN}" "https://api-gw-service-nmn.local/keycloak/admin/realms/shasta/clients/${id}" > client.json
-    if [[ ${foundArgoInRedirectUris} -ge 1 ]];then
-        echo "Argo URL is set in RedirectUris"
-        echo "${redirectUris}"
+    http_code=$(curl -s -o "client.json" -w "%{http_code}" -k -XGET -H "Authorization: Bearer ${TOKEN}" "https://api-gw-service-nmn.local/keycloak/admin/realms/shasta/clients/${id}")
+    if [[ ${http_code} -ne 200 ]]; then
+        echo "Request Failed, Response code: ${http_code}"
+        cat "${res_file}"
+        exit 1
     else
-        echo
-        cat client.json | jq ".redirectUris[.redirectUris | length] |= . + \"https://$argoUrl/oauth/callback\""  | tee client.json
+        echo "Get client json"
+    fi
+    
+    echo "Configure RedirectUris"
+    if jq '.[] | select(.clientId=="oauth2-proxy-customer-management") | .redirectUris' < "${res_file}" | grep "https://argo.cmn"; then
+        echo "  Argo URL is set in RedirectUris"
+    else
+        tmpFile=$(mktemp)
+        jq ".redirectUris[.redirectUris | length] |= . + \"https://$argoUrl/oauth/callback\"" < client.json  | tee "${tmpFile}" > /dev/null
+        mv "${tmpFile}" client.json
+    fi
+    
+    echo "Configure WebOrigins"
+    if jq '.[] | select(.clientId=="oauth2-proxy-customer-management") | .webOrigins'  < "${res_file}" | grep "https://argo.cmn"; then
+        echo "  Argo URL is set in WebOrigins"
+    else
+        tmpFile=$(mktemp)
+        jq ".webOrigins[.webOrigins | length] |= . + \"https://$argoUrl\"" < client.json  | tee "${tmpFile}"  > /dev/null
+        mv "${tmpFile}" client.json
     fi
 
-    if [[ ${foundArgoInWebOrigins} -ge 1 ]];then
-        echo "Argo URL is set in WebOrigins"
-        echo "${webOrigins}"
-    else
-        echo
-        #shellcheck disable=SC2002
-        cat client.json | jq ".webOrigins[.webOrigins | length] |= . + \"https://$argoUrl\"" | tee client.json
-    fi
-
-    curl -k -XPUT \
+    res_file=$(mktemp)
+    http_code=$(curl -s -o "${res_file}" -w "%{http_code}" -XPUT \
         -H "Authorization: Bearer ${TOKEN}" \
         -H "Content-Type: application/json" \
         -d @client.json \
-        "https://api-gw-service-nmn.local/keycloak/admin/realms/shasta/clients/${id}" > /dev/null
+        "https://api-gw-service-nmn.local/keycloak/admin/realms/shasta/clients/${id}")
+    if [[ ${http_code} -ne 204 ]]; then
+        echo "Request Failed, Response code: ${http_code}"
+        cat "${res_file}"
+        exit 1
+    fi
+    echo "Successfully configured keycloak for argo"
 }
 
 deployNLS
