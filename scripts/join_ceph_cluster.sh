d523cc5259ca299b9540467e87b41813385f2e9f
Lindsay Eliasen <lindsay.eliasen@hpe.com>
2022-12-19 23:05:18 -0600

    CASMINST-5651 replace scripts/join_ceph_cluster.sh to match script in re-add storage nodes
    (cherry picked from commit 9bfbc44a8df0567429cd8c469012f4b6c5cc535d)

--- scripts/join_ceph_cluster.sh
+++ scripts/join_ceph_cluster.sh
@@ -23,209 +23,100 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 #
 
-(( counter=0 ))
-
 host=$(hostname)
+host_ip=$(host ${host} | awk '{ print $NF }')
 
-truncate --size=0 ~/.ssh/known_hosts  2>&1
+# run preload images on host
+if [[ ! $(/srv/cray/scripts/common/pre-load-images.sh) ]]; then
+  echo "Unable to run pre-load-images.sh on $host."
+fi
 
-/srv/cray/scripts/common/pre-load-images.sh
-
-function gather_ceph_conf () {
-  FSID=$(ssh $node 'ceph -s --format=json-pretty|jq -r .fsid')
-  WAS_MON=$(ssh $node ceph node ls|jq -r --arg h $host 'any(.mon|keys; .[] == $h)')
-  if [[ "$WAS_MON" != "true" ]]; then 
-    WAS_MON="false"
+# update ssh keys for rebuilt node on host and on ncn-s001/2/3
+truncate --size=0 ~/.ssh/known_hosts 2>&1
+for node in ncn-s001 ncn-s002 ncn-s003; do
+  if ! host ${node}; then
+    echo "Unable to get IP address of $node"
+    exit 1
+  else
+    ncn_ip=$(host ${node} | awk '{ print $NF }')
   fi
-  WAS_OSD=$(ssh $node ceph node ls|jq -r --arg h $host 'any(.osd|keys; .[] == $h)')
-  if [[ "$WAS_OSD" != "true" ]]; then 
-    WAS_OSD="false"
+  # add new authorized_hosts entry for the node
+  ssh-keyscan -H "${node},${ncn_ip}" >> ~/.ssh/known_hosts
+  
+  if [[ "$host" != "$node" ]]; then
+    ssh $node "if [[ ! -f ~/.ssh/known_hosts ]]; then > ~/.ssh/known_hosts; fi; ssh-keygen -R $host -f ~/.ssh/known_hosts > /dev/null 2>&1; ssh-keygen -R $host_ip -f ~/.ssh/known_hosts > /dev/null 2>&1; ssh-keyscan -H ${host},${host_ip} >> ~/.ssh/known_hosts"
   fi
-  if [[ "$WAS_OSD" == "true" ]]
+done
+
+# copy necessary ceph files to rebuilt node
+(( counter=0 ))
+for node in ncn-s001 ncn-s002 ncn-s003; do
+  if [[ "$host" == "$node" ]]; then
+    (( counter+1 ))
+  elif [[ $(nc -z -w 10 $node 22) ]] || [[ $counter -lt 3 ]]
   then
-    #shellcheck disable=SC2207
-    OSDS+=($(ssh $node "ceph osd ls-tree $host"))
-  fi
-  CONF=$(ssh $node ceph config generate-minimal-conf)
-  echo "fsid $FSID"
-  #shellcheck disable=SC2145
-  echo "OSDS ${OSDS[@]}"
-  echo "WAS_MON $WAS_MON"
-  echo "WAS_OSD $WAS_OSD"
-
-}
-
-function apply_ceph_conf () {
-  if [[ "$WAS_OSD" == "true" ]]
-  then
-    if [[ ! -d /var/lib/ceph/$FSID ]]
+    if [[ "$host" =~ ^("ncn-s001"|"ncn-s002"|"ncn-s003")$ ]]
     then
-      if [[ ! $(mkdir /var/lib/ceph/$FSID) ]]
+      scp $node:/etc/ceph/* /etc/ceph
+    else
+      scp $node:/etc/ceph/\{rgw.pem,ceph.conf,ceph_conf_min,ceph.client.ro.keyring\} /etc/ceph/
+    fi
+             
+    if [[ ! $(pdsh -w $node "ceph orch host rm $host; ceph cephadm generate-key; ceph cephadm get-pub-key > ~/ceph.pub; ssh-copy-id -f -i ~/ceph.pub root@$host; ceph orch host add $host") ]]
+    then
+      (( counter+1 ))
+      if [[ $counter -ge 3 ]]
       then
-        echo "Unable to create /var/lib/ceph/$FSID directory"
+        echo "Unable to access ceph monitor nodes"
         exit 1
       fi
+    else
+      break
     fi
-    #shellcheck disable=SC2068
-    for osd in ${OSDS[@]}
-    do
-      if [[ ! -d /var/lib/ceph/$FSID/osd.$osd ]]
-      then
-        if [[ ! $(mkdir /var/lib/ceph/$FSID/osd.$osd) ]]
-        then
-          echo "Unable to create /var/lib/ceph/$FSID/osd.$osd directory"
-          exit 1
-        fi
-      fi
-      echo "$CONF" > /var/lib/ceph/$FSID/osd.$osd/config
-    done
   fi
-}
-
-(( loop_counter=0 ))
-(( counter_a=0 ))
-
-for node in ncn-s001 ncn-s002 ncn-s003; do
-
-  if [[ $counter -eq 0 ]] && nc -z -w 10 $node 22
-    then
-      ssh-keygen -R "$node"
-      ssh-keyscan -H "$node" >> ~/.ssh/known_hosts
-      if [[ "$host" =~ ^("ncn-s001"|"ncn-s002"|"ncn-s003")$ ]] && [[ "$host" != "$node" ]]
-      then
-        scp $node:/etc/ceph/* /etc/ceph
-      elif [[ "$host" != "$node" ]]
-      then
-        scp $node:/etc/ceph/rgw.pem /etc/ceph/rgw.pem
-      else
-        continue
-      fi
-
-      gather_ceph_conf
-      apply_ceph_conf
-
-      if [[ "$WAS_OSD" == "true" ]]
-      then
-        if [[ $counter_a -eq 0 ]] && [[ $(ssh $node "ceph orch host rm $host") ]]
-        then
-          (( counter_a+=1 ))
-        fi
-      fi
-
-      if [[ $WAS_MON == "true" ]]
-      then
-	ssh $node "ceph mon rm $host"
-      fi
-
-      if [[ ! $(ssh -o StrictHostKeyChecking=no $node "ceph cephadm generate-key; ceph cephadm get-pub-key > ~/ceph.pub; ssh-keygen -R $host; ssh-keyscan -H $host >> ~/.ssh/known_hosts ;ssh-copy-id -f -i ~/ceph.pub root@$host; ceph orch host add $host") ]]
-      then
-        if [[ "$node" =~ "ncn-s003" ]]
-        then
-          echo "Unable to access ceph monitor nodes"
-          exit 1
-        else
-          continue
-        fi
-      else
-        (( counter+=1 ))
-      fi
-  fi
+done
 
 sleep 30
-
-if [[ $WAS_OSD == "false" ]]
-then
-  (( ceph_mgr_failed_restarts=0 ))
-  (( ceph_mgr_successful_restarts=0 ))
-  until [[ $(cephadm shell -- ceph-volume inventory --format json-pretty|jq '.[] | select(.available == true) | .path' | wc -l) == 0 ]]
-  do
-      #shellcheck disable=SC2071
-      if [[ $ceph_mgr_successful_restarts > 10 ]]
+(( ceph_mgr_failed_restarts=0 ))
+(( ceph_mgr_successful_restarts=0 ))
+until [[ $(cephadm shell -- ceph-volume inventory --format json-pretty|jq '.[] | select(.available == true) | .path' | wc -l) == 0 ]]
+do
+  for node in ncn-s001 ncn-s002 ncn-s003; do
+    if [[ $ceph_mgr_successful_restarts > 10 ]]
+    then
+      echo "Failed to bring in OSDs, manual troubleshooting required."
+      exit 1
+    fi
+    if pdsh -w $node ceph mgr fail
+    then
+      (( ceph_mgr_successful_restarts+1 ))
+      sleep 120
+      break
+    else
+      (( ceph_mgr_failed_restarts+1 ))
+      if [[ $ceph_mgr_failed_restarts -ge 3 ]]
       then
-        echo "Failed to bring in OSDs, manual troubleshooting required."
+        echo "Unable to access ceph monitor nodes."
         exit 1
       fi
-      if ssh -o StrictHostKeyChecking=no $node ceph mgr fail
-      then
-        (( ceph_mgr_successful_restarts+1 ))
-        sleep 120
-        break
-      else
-        (( ceph_mgr_failed_restarts+1 ))
-        if [[ $ceph_mgr_failed_restarts -ge 3 ]]
-        then
-          echo "Unable to access ceph monitor nodes."
-          exit 1
-        fi
-      fi
+    fi
   done
-fi
-
-if $WAS_OSD
-then
-    if [[ "$host" != "$node" ]]
-    then
-      active_mgr=$(ssh $node "ceph mgr dump|jq -r '.active_name'")
-      ssh $node ceph mgr fail
-      until [[ "$active_mgr" != $(ssh $node "ceph mgr dump|jq '.active_name'") ]]
-      do
-         sleep 15
-      done
-      #shellcheck disable=SC2068
-      for osd in ${OSDS[@]}
-      do
-         echo "redeploying osd.$osd"
-         ssh $node "ceph orch daemon redeploy osd.$osd"
-         (( loop_counter+=1 ))
-         sleep 5
-     done
-     if [[ $loop_counter -ge 1 ]]
-     then
-       break
-     fi
-  fi
-fi
-echo "loop counter: $loop_counter"
 done
 
-num_storage_nodes=$(craysys metadata get num_storage_nodes)
-
-if [ ! -f "/etc/ceph/ceph.client.ro.keyring" ]
-then
-  truncate --size=0 ~/.ssh/known_hosts  2>&1
-  echo "*** num of storage nodes $num_storage_nodes"
-  for node in $(seq 1 $num_storage_nodes); do
-   nodename=$(printf "ncn-s%03d.nmn" $node)
-   echo "****** ssh-keyscan $nodename"
-   ssh-keyscan -t rsa -H $nodename >> ~/.ssh/known_hosts
-  done
-
-  for node in $(seq 1 $num_storage_nodes); do
-   nodename=$(printf "ncn-s%03d.nmn" $node)
-    if [[ $(scp $nodename:/etc/ceph/ceph.client.ro.keyring /etc/ceph/ceph.client.ro.keyring) ]]
-    then
-      break
-    fi
-  done
-fi
-
-if  [ ! -f "/etc/ceph/ceph.conf" ]
-then
-  for node in $(seq 1 $num_storage_nodes); do
-    nodename=$(printf "ncn-s%03d.nmn" $node)
-    if [[ $(scp $nodename:/etc/ceph/ceph.conf /etc/ceph/ceph.conf) ]]
-    then
-      break
-    fi
-  done
-fi
-
-for service in $(cephadm ls | jq -r '.[].systemd_unit'|grep -v cephadm)
+for service in $(cephadm ls | jq -r '.[].systemd_unit')
 do
   systemctl enable $service
-  is_active=null
-  until [[ "$is_active" == "active" ]]
-  do 
-    is_active=$(systemctl is-active $service)
-  done
 done
+
+# check rgw and haproxy are functional
+res_file=$(mktemp)
+http_code=$(curl -k -s -o "${res_file}" -w "%{http_code}" "https://rgw-vip.nmn")
+if [[ ${http_code} != 200 ]]; then
+  echo "ERROR Rados GW and haproxy are not healthy. Deploy RGW on rebuilt node."
+  exit 1
+fi
+# check keepalived is active
+if [[ $(systemctl is-active keepalived.service) != "active" ]]; then
+  echo "ERROR keepalived is not active on $host. Add node to Haproxy and Keepalived."
+  exit 1
+fi
