c370819a12657a86aa25619b73c14fb0c311563b
Kevin Burns <kevin.burns@hpe.com>
2023-01-18 09:27:06 -0600

    CASMPET-5081 Add checks for spire uninstall/install

--- upgrade/scripts/upgrade/util/upgrade-spire.sh
+++ upgrade/scripts/upgrade/util/upgrade-spire.sh
@@ -33,22 +33,22 @@ agentsvidder="${datadir}/agent_svid.der"
 jointoken="${conf}/join_token"
 
 function sshnh() {
-	/usr/bin/ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$@"
+  /usr/bin/ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$@"
 }
 
 function scpnh() {
-	/usr/bin/scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$@"
+  /usr/bin/scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$@"
 }
 
 for node in $(kubectl get nodes -o name | cut -d"/" -f2) $(ceph node ls | jq -r '.[] | keys[]' | sort -u); do
-	echo "$node: Stopping spire-agent"
-	sshnh "$node" systemctl stop spire-agent
-	sshnh "$node" rm "${svidkey}" "${bundleder}" "${agentsvidder}" "${jointoken}"
+  echo "$node: Stopping spire-agent"
+  sshnh "$node" systemctl stop spire-agent
+  sshnh "$node" rm "${svidkey}" "${bundleder}" "${agentsvidder}" "${jointoken}"
 
-	sshnh "$node" zypper install -y spire-agent
+  sshnh "$node" zypper install -y spire-agent
 
-	echo "$node: Starting spire-agent"
-	sshnh "$node" systemctl start spire-agent
+  echo "$node: Starting spire-agent"
+  sshnh "$node" systemctl start spire-agent
 done
 
 echo "Uninstalling spire helm chart"
@@ -56,7 +56,20 @@ helm uninstall -n spire spire
 echo "Uninstalling spire PVCs"
 kubectl delete -n spire pvc spire-data-spire-server-0 spire-data-spire-server-1 spire-data-spire-server-2
 
-# WAIT HERE FOR SPIRE PODS TO BE REMOVED
+RETRY=0
+MAX_RETRIES=30
+RETRY_SECONDS=5
+
+until [ "$(kubectl get pods -n spire --no-headers | wc -l)" -ne 0 ]; do
+  if [[ "$RETRY" -lt "$MAX_RETRIES" ]]; then
+    RETRY="$((RETRY + 1))"
+    echo "spire-server is not ready. Will retry after $RETRY_SECONDS seconds. ($RETRY/$MAX_RETRIES)"
+  else
+    echo "spire-server did not start after $(echo "$RETRY_SECONDS" \* "$MAX_RETRIES" | bc) seconds."
+    exit 1
+  fi
+  sleep "$RETRY_SECONDS"
+done
 
 echo "Installing new spire helm chart"
 BUILDDIR="/tmp/build"
@@ -65,9 +78,9 @@ kubectl get secrets -n loftsman site-init -o jsonpath='{.data.customizations\.ya
 manifestgen -i "${CSM_ARTI_DIR}/manifests/sysmgmt.yaml" -c "${BUILDDIR}/customizations.yaml" -o "${BUILDDIR}/spireupgrade.yaml"
 charts="$(yq r $BUILDDIR/spireupgrade.yaml 'spec.charts[*].name')"
 for chart in $charts; do
-	if [[ $chart != "spire" ]]; then
-		yq d -i $BUILDDIR/spireupgrade.yaml "spec.charts.(name==$chart)"
-	fi
+  if [[ $chart != "spire" ]]; then
+    yq d -i $BUILDDIR/spireupgrade.yaml "spec.charts.(name==$chart)"
+  fi
 done
 
 yq w -i $BUILDDIR/spireupgrade.yaml "metadata.name" "spireupgrade"
@@ -75,7 +88,20 @@ yq d -i $BUILDDIR/spireupgrade.yaml "spec.sources"
 
 loftsman ship --charts-path "${CSM_ARTI_DIR}/helm/" --manifest-path $BUILDDIR/spireupgrade.yaml
 
-# WAIT HERE FOR SPIRE TO BE HEALTHY
+RETRY=0
+MAX_RETRIES=30
+RETRY_SECONDS=10
+
+until kubectl exec -itn spire spire-server-0 --container spire-server -- ./bin/spire-server healthcheck | grep -q 'Server is healthy'; do
+  if [[ "$RETRY" -lt "$MAX_RETRIES" ]]; then
+    RETRY="$((RETRY + 1))"
+    echo "spire-server is not ready. Will retry after $RETRY_SECONDS seconds. ($RETRY/$MAX_RETRIES)"
+  else
+    echo "spire-server did not start after $(echo "$RETRY_SECONDS" \* "$MAX_RETRIES" | bc) seconds."
+    exit 1
+  fi
+  sleep "$RETRY_SECONDS"
+done
 
 echo "Joining storage nodes to spire"
 /opt/cray/platform-utils/spire/fix-spire-on-storage.sh
