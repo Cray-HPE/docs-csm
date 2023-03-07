aec2a588e5ac3b7926d5dd5ef5ea353e5f1c7fc8
phalseth-hpe <77114933+phalseth-hpe@users.noreply.github.com>
2023-03-07 14:45:52 -0600

    CASMINST-6048 (#3405)
    * add embedded repo import procedure
    
    * small tweak to path description
    
    * fix failing shell check
    
    * fix markup
    
    * fix markups
    
    * fix more markups
    
    * fix even more markups
    
    * fix spelling errors
    
    * fix trailing spaces
    
    * fix more trailing spaces
    
    * add missing licenses, fix shell check issues
    
    * fix more shell check issues
    
    * adjust path description
    
    * update embedded-repository.yaml
    
    * changes to use static embedded-repository.yaml
    
    * set executable bits on setup-embedded-repository.sh
    
    * fix versioning issue
    
    * markdown fixes per comments from Rusty
    
    * spell check fixes
    
    * use CSM_RELEASE instead of RELEASE_VERSION
    
    * Added the sickest of backticks
    
    Signed-off-by: Greg Baker <80114231+gbaker-hpe@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Greg Baker <80114231+gbaker-hpe@users.noreply.github.com>
    Co-authored-by: Greg Baker <80114231+gbaker-hpe@users.noreply.github.com>

---
+++ operations/package_repository_management/Nexus_Import_Embedded_Repo.md
@@ -0,0 +1,42 @@
+# The Embedded Repository
+
+The "embedded repo" is the complete set of packages installed on the Kubernetes and Storage Ceph NCN images,
+as well as the packages found on the PIT ISO.
+The list of installed packages in these images is queried when building each CSM release tarball,
+and a repo is created from that list and included in the CSM release tarball.
+This procedure describes how to manually install the "embedded repo" into Nexus.
+
+## Manually import the Embedded Repository into Nexus
+
+**Prerequisites:**
+
+1. The `CSM_RELEASE` version is required and will be provided as the 1st parameter to the import script.
+Example: `CSM_RELEASE="1.3.1"`
+
+1. It is necessary to have an extracted copy of the CSM release tarball available at a known path in order to proceed. This path will be provided as the 2nd parameter to the import script.
+Example: `PATH_TO_ROOT_OF_EXTRACTED_CSM_TARBALL_CONTENT="/mnt/pitdata/csm-1.3.1"`
+
+Run the following command on a master node:
+
+(`ncn-m#`)
+
+```bash
+/usr/share/doc/csm/scripts/operations/nexus/setup-embedded-repository.sh $CSM_RELEASE $PATH_TO_ROOT_OF_EXTRACTED_CSM_TARBALL_CONTENT
+```
+
+On the success, the above command will report:
+
+```bash
++ Nexus setup complete
+setup-embedded-repository.sh: OK
+```
+
+## Using the Embedded Repo
+
+In order to access the packages in the embedded repo, it is necessary to add the repository to `zypper` on each of the NCNs it will be accessed from.
+
+(`ncn-m#`)
+
+```bash
+zypper ar https://packages.local/repository/csm-${CSM_RELEASE}-embedded csm-embedded
+```
---
+++ scripts/operations/nexus/setup-embedded-repository.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+# MIT License
+#
+# (C) Copyright [2023] Hewlett Packard Enterprise Development LP
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+
+set -exo pipefail
+
+#two required inputs
+#1 CSM_RELEASE
+#2 ROOTDIR of extracted CSM tarball
+
+#CSM_RELEASE
+CSM_RELEASE="$1"
+#ROOTDIR is the root directory of the extracted CSM release tarball
+ROOTDIR="$2"
+
+if [[ -z "${CSM_RELEASE}" ]] || [[ -z "${ROOTDIR}" ]]; then
+    echo "usage: setup-embedded-repository.sh \$CSM_RELEASE \$PATH_TO_ROOT_OF_EXTRACTED_CSM_TARBALL_CONTENT"
+    exit 1
+fi
+
+BUILDDIR="$(dirname "${BASH_SOURCE[0]}")"
+TEMPLATEDIR="${BUILDDIR}/repo_templates"
+source "${ROOTDIR}/lib/version.sh"
+source "${ROOTDIR}/lib/install.sh"
+
+# Check for required resources for Nexus setup
+nexus_resources_ready=0
+counter=1
+counter_max=10
+sleep_time=30
+url=packages.local
+
+while [[ $nexus_resources_ready -eq 0 ]] && [[ "$counter" -le "$counter_max" ]]; do
+    nexus_check_configmap=$(kubectl -n services get cm cray-dns-unbound -o json 2>&1 | jq '.binaryData."records.json.gz"' -r 2>&1 | base64 -d 2>&1| gunzip - 2>&1|jq 2>&1|grep $url|wc -l)
+    nexus_check_dns=$(dig $url +short |wc -l)
+    nexus_check_pod=$(kubectl get pods -n nexus| grep nexus | grep -v Completed | awk '{ print $3 }')
+
+    if [[ "$nexus_check_dns" -eq "1" ]] && [[ "$nexus_check_pod" == "Running" ]]; then
+        echo "$url is in dns."
+        echo "Nexus pod $nexus_check_pod."
+        echo "Moving forward with Nexus setup."
+        nexus_resources_ready=1
+    fi
+    if [[ "$nexus_check_pod" != "Running" ]]; then
+        echo "Nexus pod not ready yet."
+        echo "Nexus pod status is: $nexus_check_pod."
+    fi
+
+    if [[ "$nexus_check_dns" -eq "0" ]]; then
+        echo "$url is not in DNS yet."
+        if [ "$nexus_check_configmap" -lt "1" ]; then
+            echo "$url is not loaded into unbound configmap yet."
+            echo "Waiting for DNS and nexus pod to be ready. Retry in $sleep_time seconds. Try $counter out of $counter_max."
+        fi
+    fi
+    if [[ "$counter" -eq "$counter_max" ]]; then
+        echo "Max number of checks reached, exiting."
+        echo "Please check the status of nexus, cray-dns-unbound and cray-sls."
+        exit 1
+    fi
+    ((counter++))
+done
+
+# Set podman --dns flags to unbound IP
+podman_run_flags+=(--dns "$(kubectl get -n services service cray-dns-unbound-udp-nmn -o jsonpath='{.status.loadBalancer.ingress[0].ip}')")
+
+load-install-deps
+
+# Update repository names based on the release version
+sed -e "s/-0.0.0/-${CSM_RELEASE}/g" "${TEMPLATEDIR}/embedded-repository.yaml" \
+> "${BUILDDIR}/embedded-repository.yaml"
+
+
+# Setup Nexus
+nexus-setup repositories "${BUILDDIR}/embedded-repository.yaml"
+
+# Upload repository contents
+nexus-upload raw "${ROOTDIR}/rpm/embedded" "csm-${CSM_RELEASE}-embedded"
+
+clean-install-deps
+
+set +x
+cat >&2 <<EOF
++ Nexus setup complete
+setup-embedded-repository.sh: OK
+EOF
