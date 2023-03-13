54076c95e8a3db75b49e0d86dacfd8288a3cb873
Nick Davidson <86747615+ndavidson-hpe@users.noreply.github.com>
2023-03-13 12:51:52 -0600

    missed yq3

--- upgrade/scripts/upgrade/util/update-customizations.sh
+++ upgrade/scripts/upgrade/util/update-customizations.sh
@@ -88,7 +88,7 @@ yq w -i "$c" 'spec.kubernetes.services.cray-opa.ingresses.ingressgateway-hmn.iss
 yq w -i "$c" 'spec.kubernetes.services.cray-istio.services.istio-ingressgateway-hmn.serviceAnnotations.[external-dns.alpha.kubernetes.io/hostname]' 'api.hmnlb.{{ network.dns.external }},auth.hmnlb.{{ network.dns.external }},hmcollector.hmnlb.{{ network.dns.external }}'
 
 # cray-keycloak
-if [[ -n "$(yq3 r "$c" "spec.kubernetes.services.cray-keycloak.keycloak.keycloak")" ]]; then
+if [[ -n "$(yq r "$c" "spec.kubernetes.services.cray-keycloak.keycloak.keycloak")" ]]; then
   yq r "$c" 'spec.kubernetes.services.cray-keycloak.keycloak.keycloak' | yq p - 'spec.kubernetes.services.cray-keycloak.keycloak' | yq m -i "$c" -
   yq d -i "$c" 'spec.kubernetes.services.cray-keycloak.keycloak.keycloak'
   yq w -i "$c" 'spec.kubernetes.services.cray-keycloak.keycloak.contextPath' "$(yq r "$c" 'spec.kubernetes.services.cray-keycloak.keycloak.basepath')"
