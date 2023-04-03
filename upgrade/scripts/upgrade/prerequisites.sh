c0319da9bb05020716c72360f3e928f496513fea
Bo Quan <bo.quan@hpe.com>
2023-04-03 17:49:39 -0400

    CASMINST-6152: upgrade drydock prior to kyverno in prereq

--- upgrade/scripts/upgrade/prerequisites.sh
+++ upgrade/scripts/upgrade/prerequisites.sh
@@ -505,6 +505,20 @@ else
     echo "====> ${state_name} has been completed" | tee -a "${LOG_FILE}"
 fi
 
+state_name="UPGRADE_DRYDOCK"
+state_recorded=$(is_state_recorded "${state_name}" "$(hostname)")
+if [[ ${state_recorded} == "0" && $(hostname) == "ncn-m001" ]]; then
+    echo "====> ${state_name} ..." | tee -a "${LOG_FILE}"
+    {
+
+    upgrade_csm_chart cray-drydock platform.yaml
+
+    } >> "${LOG_FILE}" 2>&1
+    record_state "${state_name}" "$(hostname)" | tee -a "${LOG_FILE}"
+else
+    echo "====> ${state_name} has been completed" | tee -a "${LOG_FILE}"
+fi
+
 state_name="UPGRADE_KYVERNO"
 state_recorded=$(is_state_recorded "${state_name}" "$(hostname)")
 if [[ ${state_recorded} == "0" && $(hostname) == "ncn-m001" ]]; then
