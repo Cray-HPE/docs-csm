6b5ac9d05ef7a528a76cfc85b67d50b5f63bf32a
Rob Noska <rnoska@hpe.com>
2023-05-19 14:31:33 -0700

    CASMPET-6584 Use yq4 to remove instances of cray-service.sqlCluster. For the case of cray-spire, remove that entry because it has no other elments.

--- upgrade/scripts/upgrade/util/update-customizations.sh
+++ upgrade/scripts/upgrade/util/update-customizations.sh
@@ -136,8 +136,12 @@ fi
 # Kube-prometheus-stack
 sed -i 's/prometheus-operator/kube-prometheus-stack/g' "$c"
 
-# When upgrading to CSM 1.5 or later, ensure that we remove obsolete cray-service.postgreSql entries (CASMPET-6584).
-yq d -i "$c" 'spec.kubernetes.services.*.cray-service.sqlCluster'
+# When upgrading to CSM 1.5 or later, ensure that we remove obsolete cray-service.sqlCluster entries (CASMPET-6584).
+yq4 -i eval 'del(.spec.kubernetes.services.*.cray-service.sqlCluster)' "$c"
+
+# Remove the cray-spire entry because the cray-spire chart is not dependent on the cray-service base chart and this
+# customization only contains cray-service.sqlcluster (CASMPET-6584).
+yq4 -i eval 'del(.spec.kubernetes.services.cray-spire)' "$c"
 
 if [[ "$inplace" == "yes" ]]; then
     cp "$c" "$customizations"
