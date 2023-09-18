0b709aadad95ad32747c94eff007ea3acd38b148
Brad Klein <pingbrad@gmail.com>
2023-09-18 14:40:02 -0600

    Update upgrade/scripts/upgrade/util/upgrade-test-rpms.sh
    Co-authored-by: Russell Bunch <doomslayer@hpe.com>
    Signed-off-by: Brad Klein <pingbrad@gmail.com>

--- upgrade/scripts/upgrade/util/upgrade-test-rpms.sh
+++ upgrade/scripts/upgrade/util/upgrade-test-rpms.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 ncns=$(grep -oP 'ncn-\w\d+' /etc/hosts | sort -u |  tr -t '\n' ',')
 
 echo "Installing updated versions of csm-testing goss-servers craycli rpms"
-pdsh -b -w ${ncns} 'zypper install -y csm-testing goss-servers craycli'
+pdsh -S -b -w ${ncns} 'zypper install -y csm-testing goss-servers craycli'
 
 echo "Enabling and starting goss-servers"
-pdsh -b -w ${ncns} 'systemctl enable goss-servers && systemctl start goss-servers'
+pdsh -S -b -w ${ncns} 'systemctl enable goss-servers && systemctl start goss-servers'
