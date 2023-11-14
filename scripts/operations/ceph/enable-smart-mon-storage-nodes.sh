33e5ae813eba993fd0e33c45a5fe1b160b1846d0
Lindsay Eliasen <lindsay.eliasen@hpe.com>
2023-11-14 08:45:24 -0600

    CASMPET-6860 apply node-exporter inside cephadm shell
    (cherry picked from commit d01d7190c509c9e14754c0a700d1683fbfdc84d7)

--- scripts/operations/ceph/enable-smart-mon-storage-nodes.sh
+++ scripts/operations/ceph/enable-smart-mon-storage-nodes.sh
@@ -112,4 +112,4 @@ for storage_node in "${ORCH_HOSTS[@]}"; do
 done
 
 echo "Reconfiguring node-exporter to publish smartmon data"
-ssh ncn-s001 ${ssh_options} "ceph orch apply -i /etc/cray/ceph/node-exporter.yml"
+ssh ncn-s001 ${ssh_options} "cephadm shell --mount /etc/cray/ceph/ -- ceph orch apply -i /mnt/node-exporter.yml" 
