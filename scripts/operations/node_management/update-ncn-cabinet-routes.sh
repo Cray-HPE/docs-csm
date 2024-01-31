b78e2b87f380c1067ce650a100d390302478a99b
Russell Bunch <doomslayer@hpe.com>
2024-01-31 17:12:59 -0600

    fixing licenses

--- scripts/operations/node_management/update-ncn-cabinet-routes.sh
+++ scripts/operations/node_management/update-ncn-cabinet-routes.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/env bash
+#
 # MIT License
 #
-# (C) Copyright [2022] Hewlett Packard Enterprise Development LP
+# (C) Copyright 2022, 2024 Hewlett Packard Enterprise Development LP
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -20,7 +21,7 @@
 # OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
-
+#
 URL="https://api-gw-service-nmn.local/apis/sls/v1/networks"
 
 function on_error() {
