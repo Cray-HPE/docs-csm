feb715e4ee22fe5b81f2ae1354472f9dd8cab8c8
Mitch Harding (the weird one) <mitchell.harding@hpe.com>
2024-04-24 17:22:58 -0400

    CASMINST-6851: Linting
    (cherry picked from commit bcdc76f42d765b78ed53ac3850a786f5716ea4cf)

--- scripts/operations/configuration/import_bos_data.py
+++ scripts/operations/configuration/import_bos_data.py
@@ -131,7 +131,7 @@ def get_options_to_change(options_to_import: BosOptions, current_options: BosOpt
 
 def change_options(option_data: BosOptions, option_names_to_change: List[str]) -> None:
     """
-    Create a dicitonary mapping the specified options to be changed to the new values that
+    Create a dictionary mapping the specified options to be changed to the new values that
     they should have. Then update the BOS options accordingly.
     """
     if not option_names_to_change:
