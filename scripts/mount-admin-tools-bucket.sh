0c111520220702654f28889977300830f30091e0
Lindsay Eliasen <87664908+leliasen-hpe@users.noreply.github.com>
2024-08-30 10:04:16 -0500

    CASMPET-7209 Apply script improvement suggestions from code review
    Co-authored-by: Russell Bunch <doomslayer@hpe.com>
    Signed-off-by: Lindsay Eliasen <87664908+leliasen-hpe@users.noreply.github.com>
    (cherry picked from commit 35b51cc5e40601a1b3b1664c485d5f9ed9a0d96d)

--- scripts/mount-admin-tools-bucket.sh
+++ scripts/mount-admin-tools-bucket.sh
@@ -20,13 +20,12 @@
 # OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
-
+set -euo pipefail
 # Mount admin-tools S3 bucket
-s3_bucket="admin-tools"
-s3fs_mount_dir="/var/lib/admin-tools"
-s3_user="admin-tools"
-
-s3fs_cache_dir=/var/lib/s3fs_cache
+s3_bucket="${S3_BUCKET:-admin-tools}"
+s3fs_mount_dir="${S3FS_MOUNT_DIR:-/var/lib/admin-tools}"
+s3_user="${S3_USER:-admin-tools}"
+s3fs_cache_dir="${S3FS_CACHE_DIR:-/var/lib/s3fs_cache}"
 if [ -d ${s3fs_cache_dir} ]; then
   s3fs_opts="use_path_request_style,use_cache=${s3fs_cache_dir},check_cache_dir_exist,use_xattr"
 else
@@ -35,12 +34,28 @@ fi
 
 echo "Configuring for ${s3_bucket} S3 bucket at ${s3fs_mount_dir} for ${s3_user} S3 user"
 
-mkdir -p ${s3fs_mount_dir}
+if [ ! -d "${s3fs_mount_dir}" ]; then
+  mkdir -pv "${s3fs_mount_dir}"
+fi
 
-pwd_file=/root/.${s3_user}.s3fs
-access_key=$(kubectl get secret ${s3_user}-s3-credentials -o json | jq -r '.data.access_key' | base64 -d)
-secret_key=$(kubectl get secret ${s3_user}-s3-credentials -o json | jq -r '.data.secret_key' | base64 -d)
-s3_endpoint=$(kubectl get secret ${s3_user}-s3-credentials -o json | jq -r '.data.http_s3_endpoint' | base64 -d)
+pwd_file="/root/.${s3_user}.s3fs"
+secret_name="${s3_user}-s3-credentials"
+s3_user_credentials="$(
+  if ! kubectl get secret "$secret_name" -o json 2>/dev/null | jq -r '.data' 2>/dev/null; then
+    echo >&2 "Failed to obtain credential data for user: [$s3_user]"
+  fi
+)"
+if [ -z "$s3_user_credentials" ]; then
+  echo "Exiting."
+  exit 1
+fi
+access_key="$(jq -n -r --argjson s3_user_credentials "$s3_user_credentials" '$s3_user_credentials.access_key' | base64 -d)"
+secret_key="$(jq -n -r --argjson s3_user_credentials "$s3_user_credentials" '$s3_user_credentials.secret_key' | base64 -d)"
+s3_endpoint="$(jq -n -r --argjson s3_user_credentials "$s3_user_credentials" '$s3_user_credentials.http_s3_endpoint' | base64 -d)"
+if [ "$access_key" = 'null' ] || [ "$secret_key" = 'null' ] || [ "$s3_endpoint" = 'null' ]; then
+  echo >&2 "Failed to find access_key, secret_key, or http_s3_endpoint for [$s3_user]"
+  exit 1
+fi
 
 echo "${access_key}:${secret_key}" > ${pwd_file}
 chmod 600 ${pwd_file}
@@ -49,23 +64,23 @@ echo "Mounting bucket: ${s3_bucket} at ${s3fs_mount_dir}"
 s3fs ${s3_bucket} ${s3fs_mount_dir} -o passwd_file=${pwd_file},url=${s3_endpoint},${s3fs_opts}
 
 echo "Adding fstab entry for ${s3_bucket} S3 bucket at ${s3fs_mount_dir} for ${s3_user} S3 user"
-if [[ -z $(cat /etc/fstab | grep "admin-tools" | grep "fuse.s3fs") ]]; then
+if grep "${s3_bucket}" /etc/fstab | grep -q "fuse.s3fs"; then
   echo "${s3_bucket} ${s3fs_mount_dir} fuse.s3fs _netdev,allow_other,passwd_file=${pwd_file},url=${s3_endpoint},${s3fs_opts} 0 0" >> /etc/fstab
 else
   echo "An entry in /etc/fstab already exists for ${s3_bucket} ${s3fs_mount_dir} fuse.s3fs"
 fi
 
 echo "Set cache pruning for admin tools to 5G of the 200G volume (every 2nd hour)"
-echo "0 */2 * * * root /usr/bin/prune-s3fs-cache.sh admin-tools ${s3fs_cache_dir} 5368709120 -silent" > /etc/cron.d/prune-s3fs-admin-tools-cache
+echo "0 */2 * * * root /usr/bin/prune-s3fs-cache.sh ${s3_bucket} ${s3fs_cache_dir} 5368709120 -silent" > /etc/cron.d/prune-s3fs-${s3_bucket}-cache
 
-echo -e "Done mounting admin-tools S3 bucket\n"
+echo -e "Done mounting ${s3_bucket} S3 bucket\n"
 
 # Validate admin-tools S3 bucket has been mounted
 
 echo "/etc/fstab has the following content:"
 grep fuse.s3fs /etc/fstab
 exit_code=0
-if [[ -n $(cat /etc/fstab | grep "admin-tools" | grep "fuse.s3fs") ]]; then
+if grep "$s3_bucket" /etc/fstab | grep -q "fuse.s3fs"; then
   echo -e "admin-tools was successfully added to /etc/fstab\n"
 else
   echo -e "Error: admin-tools fuse.s3fs mount was not added to the /etc/fstab file\n"
@@ -73,14 +88,16 @@ else
 fi
 echo "The following s3fs mounts exist:"
 mount | grep 's3fs on'
-if [[ -n $(mount | grep 's3fs on /var/lib/admin-tools') ]]; then
-  echo -e "/var/lib/admin-tools is a s3fs mount\n"
+if mount | grep -q 's3fs on '"$s3fs_mount_dir"'; then
+  echo -e "$s3fs_mount_dir is an s3fs mount\n"
 else
-  echo -e "Error: /var/lib/admin-tools is not a s3fs mount.\n"
+  echo -e "Error: ${s3fs_mount_dir} is not an s3fs mount.\n"
   exit_code=1
 fi
 
-if [[ $exit_code == 0 ]]; then
-  echo "Successfully mounted admin-tools bucket"
+if [ "$exit_code" -eq 0 ]; then
+  echo "Successfully mounted ${s3_bucket} bucket"
+else
+  echo "Errors encountered. Please review script output."
 fi
 exit $exit_code
