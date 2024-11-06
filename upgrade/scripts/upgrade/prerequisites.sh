cbc845ee82b94b1c97edca646ea7fc0bd60bd0e4
Bo Quan <bo.quan@hpe.com>
2024-11-06 11:55:58 -0500

    CASMTRIAGE-7458: pre-upgrade cms-ipxe to be compatible with newer istio

--- upgrade/scripts/upgrade/prerequisites.sh
+++ upgrade/scripts/upgrade/prerequisites.sh
@@ -892,6 +892,8 @@ do_upgrade_csm_chart cray-drydock platform.yaml
 do_upgrade_csm_chart cray-sysmgmt-health platform.yaml
 do_upgrade_csm_chart cray-tftp sysmgmt.yaml
 do_upgrade_csm_chart cray-tftp-pvc sysmgmt.yaml
+# cms-ipxe needs to be upgraded here so it is compatible with the newer istio version in CSM 1.6
+do_upgrade_csm_chart cms-ipxe sysmgmt.yaml
 # cray-product-catalog needs to be upgraded here in CSM 1.6 so it is compatible with SAT when building images/cfs configs
 do_upgrade_csm_chart cray-product-catalog sysmgmt.yaml
 
