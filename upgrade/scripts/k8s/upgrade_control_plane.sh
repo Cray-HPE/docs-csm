fa814509ac95b874e2c30bb58846dbfcbca084fb
Russell Bunch <doomslayer@hpe.com>
2025-01-07 11:44:22 -0600

    Revert "Revert "Removes double-space""
    This reverts commit 44683295663ff1348f7d6d5bc95cd235f766041d.

--- upgrade/scripts/k8s/upgrade_control_plane.sh
+++ upgrade/scripts/k8s/upgrade_control_plane.sh
@@ -110,6 +110,6 @@ for master in $masters; do
     exit 1
   fi
   echo ""
-  echo "Successfully upgraded  apiserver-etcd-client certificate for $master."
+  echo "Successfully upgraded apiserver-etcd-client certificate for $master."
   echo ""
 done
