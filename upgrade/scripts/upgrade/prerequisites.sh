edd12e07fa8f57ac9dbf26c12db214c5117aaf9b
Lindsay Eliasen <87664908+leliasen-hpe@users.noreply.github.com>
2025-03-04 10:34:18 -0600

    CASMPET-7409 add sleep after deploying postgres-operator crds in prerequisites.sh (#5780)

--- upgrade/scripts/upgrade/prerequisites.sh
+++ upgrade/scripts/upgrade/prerequisites.sh
@@ -568,8 +568,15 @@ if [[ ${state_recorded} == "0" && $(hostname) == "${PRIMARY_NODE}" ]]; then
       echo "Error: failed to find cray-postgres-operator chart in ${CSM_ARTI_DIR}/helm."
       exit 1
     fi
-    # create CRDs for cray-postgres-operator, this is necessary when postgres is upgraded to 1.10.1 in CSM 1.7
-    tar --extract --file="$postgres_chart_path" --to-stdout cray-postgres-operator/files/postgres-operator-crds-1.10.1.yaml | kubectl apply -f -
+    # check if file exists before applying crds, needed for backwards compatibility
+    if tar -tf "$postgres_chart_path" cray-postgres-operator/files/postgres-operator-crds-1.10.1.yaml > /dev/null 2>&1; then
+      # create CRDs for cray-postgres-operator, this is necessary when postgres is upgraded to 1.10.1 in CSM 1.7
+      tar --extract --file="$postgres_chart_path" --to-stdout cray-postgres-operator/files/postgres-operator-crds-1.10.1.yaml | kubectl apply -f -
+      # 5 second sleep is necessary for cray-postgres-operator chart deploy. Chart fails with CRD error if no sleep
+      sleep 5
+    else
+      echo "File 'cray-postgres-operator/files/postgres-operator-crds-1.10.1.yaml' does not exist in $postgres_chart_path"
+    fi
   } >> "${LOG_FILE}" 2>&1
   record_state "${state_name}" "$(hostname)" | tee -a "${LOG_FILE}"
 else
