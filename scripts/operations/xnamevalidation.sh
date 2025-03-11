7b0a9bd16d0fee14127479a46e854fb4e846d96c
Bo Quan <bo.quan@hpe.com>
2025-03-11 11:04:43 -0400

    CASMTRIAGE-7947: improve xname validation script and instructions

--- scripts/operations/xnamevalidation.sh
+++ scripts/operations/xnamevalidation.sh
@@ -2,7 +2,7 @@
 #
 # MIT License
 #
-# (C) Copyright 2021-2023 Hewlett Packard Enterprise Development LP
+# (C) Copyright 2021-2023, 2025 Hewlett Packard Enterprise Development LP
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -106,7 +106,7 @@ run_loftsman() {
   loftsman ship --charts-path "${PWD}/helm" --manifest-path "${TMPDIR}/manifest.yaml"
 
   # Restart opa pods so that the policy changes are picked up
-  if kubectl get deployment -n opa |grep cray-opa-ingressgateway; then
+  if kubectl get deployment -n opa | grep cray-opa-ingressgateway; then
     # CSM 1.5.0
     kubectl rollout restart -n opa deployment cray-opa-ingressgateway
     kubectl rollout restart -n opa deployment cray-opa-ingressgateway-hmn
@@ -114,7 +114,7 @@ run_loftsman() {
     kubectl rollout restart -n opa deployment cray-opa-ingressgateway-customer-user
   fi
 
-  if kubectl get daemonset -n opa |grep cray-opa-ingressgateway; then
+  if kubectl get daemonset -n opa | grep cray-opa-ingressgateway; then
     # CSM 1.5.1 or later
     kubectl rollout restart -n opa daemonset cray-opa-ingressgateway
     kubectl rollout restart -n opa daemonset cray-opa-ingressgateway-hmn
@@ -125,6 +125,12 @@ run_loftsman() {
 
 # validate_prereqs makes sure everything is available for this script to work
 validate_prereqs() {
+  # validate running directory is correct
+  if ! [[ -d "${PWD}/helm" && -d "${PWD}/manifests" ]]; then
+    echo "Error: helm or manifests directory is not found under ${PWD}. Make sure you run this from the extracted CSM tar file."
+    exit 3
+  fi
+
   # validate site-init secret exists
   if ! kubectl get secret -n loftsman site-init > /dev/null 2>&1; then
     echo "Error: missing site-init secret in loftsman namespace."
@@ -146,12 +152,6 @@ validate_prereqs() {
   # validate that cray-spire is included in sysmgmt.yaml
   if ! yq r "${PWD}/manifests/sysmgmt.yaml" 'spec.charts(name==cray-spire)' | grep -q cray-spire; then
     echo "The cray-spire chart is missing from ${PWD}/manifests/sysmgmt.yaml"
-    exit 3
-  fi
-
-  # validate helm chart exists
-  if ! [ -d "$PWD/helm" ]; then
-    echo "Error: $PWD/helm is missing. Make sure you run this from the extracted CSM tar file"
     exit 3
   fi
 
