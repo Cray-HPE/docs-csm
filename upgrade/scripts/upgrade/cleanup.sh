51c5697c2a4fc3c77fe97479ec86b170add285d1
David Fluck <david.fluck@hpe.com>
2025-07-08 12:55:37 -0700

    CASMTRIAGE-8378: delete upgrade file in post-upgrade cleanup (#6100)
    * * Delete /etc/cray/kubernetes/upgrade file at the end of the
      post-upgrade cleanup process. BSS will ensure that this file doesn't
      get recreated on boot, but the file will continue to exist until a
      reboot, unless we delete it. This ensures that the file doesn't exist
      longer than it has to.
    
    * * Remove upgrade file from all NCNs.
    
    * Fix NCN selection regular expression.

--- upgrade/scripts/upgrade/cleanup.sh
+++ upgrade/scripts/upgrade/cleanup.sh
@@ -31,3 +31,7 @@ TOKEN=$(curl -s -S -d grant_type=client_credentials \
 export TOKEN
 
 python3 /usr/share/doc/csm/upgrade/scripts/upgrade/cleanup.py
+
+# Remove the upgrade file from all NCN hosts.
+ncns=$(grep -oP 'ncn-\w\d+' /etc/hosts | sort -u | tr -t '\n' ',')
+pdsh -S -b -w ${ncns} "rm -f /etc/cray/kubernetes/upgrade"
