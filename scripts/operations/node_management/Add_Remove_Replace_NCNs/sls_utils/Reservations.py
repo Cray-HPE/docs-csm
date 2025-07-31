5049c15d71f7acb8a5382d9b9888c303f378b810
Russell Bunch <doomslayer@hpe.com>
2025-07-31 11:38:20 -0500

    CASMNET-2345 Preserve IPv6 SLS data
    This change makes the `sls_utils` module aware of the new IPv6 enabled SLS data structure, and ensures that data will persist after SLS updates.
    
    This also updates the `add_management_ncn.py` script for the add/remove NCN procedure to preserve IPv6 data during its run. This does not add new IPv6 data for a new node, we will use CSI for that.

--- scripts/operations/node_management/Add_Remove_Replace_NCNs/sls_utils/Reservations.py
+++ scripts/operations/node_management/Add_Remove_Replace_NCNs/sls_utils/Reservations.py
@@ -1,24 +1,24 @@
-# MIT License
+#  MIT License
 #
-# (C) Copyright [2022] Hewlett Packard Enterprise Development LP
+#  (C) Copyright 2022, 2025 Hewlett Packard Enterprise Development LP
 #
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the "Software"),
-# to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation
+#  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#  and/or sell copies of the Software, and to permit persons to whom the
+#  Software is furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included
-# in all copies or substantial portions of the Software.
+#  The above copyright notice and this permission notice shall be included
+#  in all copies or substantial portions of the Software.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-# OTHER DEALINGS IN THE SOFTWARE.
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+#  OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+#  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#  OTHER DEALINGS IN THE SOFTWARE.
 """Class for managing SLS Reservations."""
 import ipaddress
 
@@ -26,17 +26,22 @@ import ipaddress
 class Reservation:
     """Name, IPv4 address and list of aliases to create A Records and CNAMEs."""
 
-    def __init__(self, name, ipv4_address, aliases=list, comment=""):
+    __ipv6_address = None
+
+    def __init__(self, name, ipv4_address, ipv6_address = None, aliases=list, comment=""):
         """Create a Reservation class.
 
         Args:
             name (str): Name of the reservation
-            ipv4_address (str): IPv4 address of the reservation
+            ipv4_address (str or ipaddress.IPv4Address): IPv4 address of the reservation
+            ipv6_address (str or ipaddress.IPv6Address): IPv6 address of the reservation
             aliases (list): List of strings for reservation alternate names
             comment (str): Arbitrary comment on the reservation
         """
         self.__name = name
         self.__ipv4_address = ipaddress.IPv4Address(ipv4_address)
+        if ipv6_address is not None:
+            self.__ipv6_address = ipaddress.IPv6Address(ipv6_address)
         self.__aliases = aliases
         self.__comment = comment
 
@@ -57,14 +62,31 @@ class Reservation:
         """IPv4 Address of the host.
 
         Args:
-            reservation_ipv4_address: IPv4 address of the reservation
+            reservation_ipv4_address (str or ipaddress.IPv4Address): IPv4 address of the reservation
 
         Returns:
             ipv4_address (ipaddress.IPv4Address):  IPv4 CIDR of the reservation.
         """
-        if reservation_ipv4_address:
+        if isinstance(reservation_ipv4_address, ipaddress.IPv4Address):
+            self.__ipv4_address = reservation_ipv4_address
+        elif isinstance(reservation_ipv4_address, str):
             self.__ipv4_address = ipaddress.IPv4Address(reservation_ipv4_address)
         return self.__ipv4_address
+
+    def ipv6_address(self, reservation_ipv6_address=None):
+        """IPv4 Address of the host.
+
+        Args:
+            reservation_ipv6_address (str or ipaddress.IPv6Address): IPv4 address of the reservation
+
+        Returns:
+            ipv4_address (ipaddress.IPv6Address):  IPv4 CIDR of the reservation.
+        """
+        if isinstance(reservation_ipv6_address, ipaddress.IPv6Address):
+            self.__ipv6_address = reservation_ipv6_address
+        elif isinstance(reservation_ipv6_address, str):
+            self.__ipv6_address = ipaddress.IPv6Address(reservation_ipv6_address)
+        return self.__ipv6_address
 
     def comment(self, reservation_comment=None):
         """Arbitrary descriptive text.
@@ -102,6 +124,8 @@ class Reservation:
             "Name": self.__name,
             "IPAddress": str(self.__ipv4_address),
         }
+        if self.__ipv6_address:
+            sls.update({"IPAddress6": str(self.__ipv6_address)})
         if self.__aliases:
             sls.update({"Aliases": [str(a) for a in self.__aliases]})
         if self.__comment is not None:
