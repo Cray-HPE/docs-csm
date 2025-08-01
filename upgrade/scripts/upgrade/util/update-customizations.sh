e810b93c373c503985d969cbb5dc360510ced8d3
Arka Pramanik <arka.pramanik@hpe.com>
2025-08-02 01:59:59 +0530

    CASMTRIAGE-8555: Imports rack-resiliency settings during upgrade (#6179)
    * Imports rack-resiliency settings during upgrade
    
    Adds rack-resiliency configuration import from upgrade customizations file since this service did not have configurable customization prior to version 1.7.
    
    * Adding condition to see if file already exist
    
    * Fix name
    
    * Apply suggestions from code review
    
    Co-authored-by: Mitch Harding <mitchell.harding@hpe.com>
    Signed-off-by: Keshav Varshney <keshav.varshney@hpe.com>
    
    ---------
    
    Signed-off-by: Keshav Varshney <keshav.varshney@hpe.com>
    Co-authored-by: keshav-06-hpe <keshav.varshney@hpe.com>
    Co-authored-by: Mitch Harding <mitchell.harding@hpe.com>

--- upgrade/scripts/upgrade/util/update-customizations.sh
+++ upgrade/scripts/upgrade/util/update-customizations.sh
@@ -125,6 +125,11 @@ if [ "$(yq4 eval '.spec.kubernetes.services."image-verification-policy"' "${upgr
   ' "$c"
 fi
 
+# rack-resiliency did not exist prior to 1.7. Import rack-resiliency settings from upgrade customizations file during upgrade.
+if [ "$(yq4 eval '.spec.kubernetes.services.rack-resiliency' "$c")" == null ]; then
+  yq4 -i eval ".spec.kubernetes.services[\"rack-resiliency\"] += (load(\"${upgrade_customizations}\") | .spec.kubernetes.services[\"rack-resiliency\"])" "$c"
+fi
+
 # Add entries for Cilium Hubble observability GUI (CASMNET-2194)
 yq4 -i eval '.spec.kubernetes.services.["cray-hubble"].externalHostname = "hubble.cmn.{{ network.dns.external }}"' "$c"
 yq4 -i eval ".spec.proxiedWebAppExternalHostnames.customerManagement |= (. + [\"{{ kubernetes.services['cray-hubble'].externalHostname }}\"] | unique)" "$c"
